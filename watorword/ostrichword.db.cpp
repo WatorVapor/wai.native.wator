996289429b2245e6ed3bc4c1ac752b4e23ebda0d
Wator Vapor <watorvapor@gmail.com>
2017-07-23 12:21:31 +0900

    Update ostrichword.db.cpp

--- watorword/ostrichword.db.cpp
+++ watorword/ostrichword.db.cpp
@@ -17,40 +17,41 @@ using namespace std;
 
 void OstrichWord::upWordByArticle2Master(const pt::ptree &task) {
   DUMP_VAR(multiWordOfOneArticle_.size());
+  pt::ptree upTask = task;
   map<int,vector<string>> localMultiWordRank;
   for(auto wordSum:multiWordOfOneArticle_) {
+    TRACE_VAR(wordSum.first,wordSum.second);
+    if(wordSum.second > 2) {
       TRACE_VAR(wordSum.first,wordSum.second);
-      if(wordSum.second > 2) {
-          TRACE_VAR(wordSum.first,wordSum.second);
-          auto it = localMultiWordRank.find(wordSum.second);
-          if(it != localMultiWordRank.end()){
-              it->second.push_back(wordSum.first);
-          } else {
-              localMultiWordRank[wordSum.second] ={wordSum.first};
-          }
+      auto it = localMultiWordRank.find(wordSum.second);
+      if(it != localMultiWordRank.end()){
+        it->second.push_back(wordSum.first);
+      } else {
+        localMultiWordRank[wordSum.second] ={wordSum.first};
       }
+    }
   }
   DUMP_VAR(localMultiWordRank.size());
-    for(auto &record:localMultiWordRank) {
-        TRACE_VAR(record.first);
-        auto words = record.second;
-        for(auto word:words){
-            bool isShort= false;
-            for(const auto &word2:words){
-                auto found = word2.find(word);
-                if(found != string::npos && word2 != word){
-                    TRACE_VAR(word);
-                    TRACE_VAR(word2);
-                    isShort= true;
-                }
-            }
-            if(isShort == false) {
-                if(stub_) {
-                } else {
-                 dict_->putWord(word,record.first);
-                }
-            }
+  for(auto &record:localMultiWordRank) {
+    TRACE_VAR(record.first);
+    auto words = record.second;
+    for(auto word:words) {
+      bool isShort= false;
+      for(const auto &word2:words){
+        auto found = word2.find(word);
+        if(found != string::npos && word2 != word){
+          TRACE_VAR(word);
+          TRACE_VAR(word2);
+          isShort= true;
         }
+      }
+      if(isShort == false) {
+      }
     }
+  }
+  std::stringstream ssTask;
+  pt::write_json(ssTask,upTask);
+  DUMP_VAR(ssTask.str());
+  
   multiWordOfOneArticle_.clear();
 }
