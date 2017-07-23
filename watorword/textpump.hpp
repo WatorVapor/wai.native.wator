890bb8b19990adf0c6db3b0d9f008ccf798c647d
Wator Vapor <watorvapor@gmail.com>
2017-07-23 11:15:38 +0900

    Update textpump.hpp

--- watorword/textpump.hpp
+++ watorword/textpump.hpp
@@ -86,36 +86,14 @@ public:
     }
   }  
   template<typename T> void eachTextFromMaster(T fn){
-    DUMP_VAR2(url_,tag_);
+    namespace pt = boost::property_tree;
+   DUMP_VAR2(url_,tag_);
     //while(true)
     {
       pt::ptree task;
       string content;
       if(fetchMasterTask(task,content)) {
         fn(task,content);
-      }
-      string taskJSONPath = "/tmp/wai.native/result.json";
-      string wget =  "wget -6 ";
-      wget += url_; 
-      wget += "/";
-      wget += tag_;
-      wget += " -O ";
-      wget += taskJSONPath;
-      DUMP_VAR(wget);
-      ::system(wget.c_str());
-      try {
-        pt::ptree taskJson;
-        pt::read_json(taskJSONPath, taskJson);
-        string taskURL;
-        auto taskURLOpt = taskJson.get_optional<string>("url");
-        if(taskURLOpt) {
-          taskURL = taskURLOpt.get();
-        }
-      } catch (const pt::json_parser::json_parser_error& e) {
-        DUMP_VAR(e.what());
-      }
-      catch( const std::exception & ex ) {
-        DUMP_VAR(ex.what());
       }
     }
   }
