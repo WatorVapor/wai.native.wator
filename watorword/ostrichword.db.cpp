eb98b0b7dc75186690b2c491a963d8d27a3ede4c
Wator Vapor <watorvapor@gmail.com>
2017-07-23 22:57:26 +0900

    Update ostrichword.db.cpp

--- watorword/ostrichword.db.cpp
+++ watorword/ostrichword.db.cpp
@@ -58,7 +58,7 @@ void OstrichWord::upWordByArticle2Master(const pt::ptree &task) {
   wordArrays += "{}";
   upTask.put("word",wordArrays);
   std::stringstream ssTask;
-  pt::write_json(ssTask,upTask);
+  pt::write_json(ssTask,upTask,false);
   DUMP_VAR(ssTask.str());
   
   multiWordOfOneArticle_.clear();
