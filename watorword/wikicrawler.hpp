f52d5c7c2ddf6cda998e09f141659a0e9f51f53d
Wator Vapor <watorvapor@gmail.com>
2017-07-25 22:20:29 +0900

    Update wikicrawler.hpp

--- watorword/wikicrawler.hpp
+++ watorword/wikicrawler.hpp
@@ -1,1 +1,24 @@
+#include <string>
+#include <tuple>
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+#pragma once
 
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+#include <boost/foreach.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/optional.hpp>
+namespace pt = boost::property_tree;
+#include "log.hpp"
+
+class WikiCrawler {
+public:
+  WikiCrawler();
+  ~WikiCrawler();
+private:
+private:
+};
