01bda8fe98f2f81f405e07116f525484ee3dde82
Wator Vapor <watorvapor@gmail.com>
2017-07-25 09:45:05 +0900

    Update ostrichword.db.cpp

--- watorword/ostrichword.db.cpp
+++ watorword/ostrichword.db.cpp
@@ -15,6 +15,51 @@ using namespace std;
 #include "log.hpp"
 
 
+void OstrichWord::mergeWordByArticle(void){
+  DUMP_VAR(multiWordOfOneArticle_.size());
+  map<int,vector<string>> localMultiWordRank;
+  for(auto wordSum:multiWordOfOneArticle_) {
+      TRACE_VAR(wordSum.first,wordSum.second);
+      if(wordSum.second > 2) {
+          TRACE_VAR(wordSum.first,wordSum.second);
+          auto it = localMultiWordRank.find(wordSum.second);
+          if(it != localMultiWordRank.end()){
+              it->second.push_back(wordSum.first);
+          } else {
+              localMultiWordRank[wordSum.second] ={wordSum.first};
+          }
+      }
+  }
+  DUMP_VAR(localMultiWordRank.size());
+    for(auto &record:localMultiWordRank) {
+        TRACE_VAR(record.first);
+        auto words = record.second;
+        for(auto word:words){
+            bool isShort= false;
+            for(const auto &word2:words){
+                auto found = word2.find(word);
+                if(found != string::npos && word2 != word){
+                    TRACE_VAR(word);
+                    TRACE_VAR(word2);
+                    isShort= true;
+                }
+            }
+            if(isShort == false) {
+                if(stub_) {
+                } else {
+                 dict_->putWord(word,record.first);
+                }
+            }
+        }
+    }
+  multiWordOfOneArticle_.clear();
+  if(stub_) {
+  } else {
+    dict_->writeDB();
+  }
+}
+
+
 void OstrichWord::upWordByArticle2Master(const pt::ptree &task) {
   DUMP_VAR(multiWordOfOneArticle_.size());
   pt::ptree upTask = task;
