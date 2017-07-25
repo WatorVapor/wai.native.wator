6db472b021e522b47ddd8aed6cda996059cf263f
Wator Vapor <watorvapor@gmail.com>
2017-07-25 22:27:46 +0900

    Update ostrich.cpp

--- tools/ostrich.cpp
+++ tools/ostrich.cpp
@@ -58,7 +58,6 @@ int main(int ac,char*av[])
 #include <boost/optional.hpp>
 namespace pt = boost::property_tree;
 
-void parseURL(const pt::ptree &task,string &content);
 int main(int ac,char*av[])
 {
   OstrichWord ostrich;
@@ -73,78 +72,8 @@ int main(int ac,char*av[])
     claw.claw(content);
     claw.eachMultiByte(learnOstrich);
     ostrich.upWordByArticle2Master(task);
-    parseURL(task,content);
   };
   TextPump txtPump("https://www.wator.xyz/wai/text/train/ostrich","tain.one");
   txtPump.eachTextFromMaster(clawText);
   return 0;
 }
-
-
-#include <boost/xpressive/xpressive.hpp>
-#include <boost/xpressive/xpressive_static.hpp>
-#include <boost/regex.hpp>
-
-#include <regex>
-
-const static string strMatchHref("href=\"");
-void parseURL(const pt::ptree &task,string &content) {
-  //DUMP_VAR(content);
-  std::regex rx( "<a.*?href=['|\"](.*?)['|\"]" );
-  string prefix;
-  string base_url;
-  try {
-    auto prefixOpt = task.get_optional<string>("prefix");
-    if(prefixOpt) {
-      prefix = prefixOpt.get();
-      DUMP_VAR(prefix);
-    }
-    auto base_urlOpt = task.get_optional<string>("base_url");
-    if(base_urlOpt) {
-      base_url = base_urlOpt.get();
-      DUMP_VAR(base_url);
-    }
-  } catch (const pt::json_parser::json_parser_error& e) {
-    DUMP_VAR(e.what());
-  }
-  catch( const std::exception & ex ) {
-    DUMP_VAR(ex.what());
-  }
-
-  
-  string crawlerArrays;
-  for(auto it = std::sregex_iterator(content.begin(), content.end(), rx);
-      it != std::sregex_iterator();
-       ++it) {
-    TRACE_VAR(it->position());
-    std::smatch match = *it;
-    auto match_href = match.str();
-    TRACE_VAR(match_href);
-    auto first = match_href.find(strMatchHref);
-    auto last = match_href.find_last_of("\"");
-    if(first != std::string::npos && last != std::string::npos && last > first){
-      string href = match_href.substr (first + strMatchHref.size(),last-first -strMatchHref.size());
-      TRACE_VAR(href);
-      auto first = href.find(prefix);
-      if(first == 0) {
-        href = base_url + href;
-        TRACE_VAR(prefix,href);
-        crawlerArrays += "{";
-        crawlerArrays += href;
-        crawlerArrays += "};";
-      }
-    }
-  }
-  pt::ptree upTask(task);
-  upTask.put("crawler",crawlerArrays);
-  string task_url_upPath = "/tmp/wai.native/task_url_up.json";
-  pt::write_json(task_url_upPath,upTask);
-  string wgetTaskUp("curl -6 -F \"");
-  //string wgetTaskUp("curl -F \"");
-  wgetTaskUp += "file=@";
-  wgetTaskUp += task_url_upPath;
-  wgetTaskUp += "\" ";
-  wgetTaskUp += "\"https://www.wator.xyz/wai/text/train/ostrich/url/tain.one\"";
-  DUMP_VAR(wgetTaskUp);
-}
-
