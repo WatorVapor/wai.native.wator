0ba3d53370f459990bb1f2c33d6280708aefeac8
Wator Vapor <watorvapor@gmail.com>
2017-07-25 09:56:46 +0900

    Update ostrichword.db.cpp

--- watorword/ostrichword.db.cpp
+++ watorword/ostrichword.db.cpp
@@ -61,46 +61,7 @@ void OstrichWord::mergeWordByArticle(void){
 
 
 void OstrichWord::upWordByArticle2Master(const pt::ptree &task) {
-  DUMP_VAR(multiWordOfOneArticle_.size());
   pt::ptree upTask = task;
-  map<int,vector<string>> localMultiWordRank;
-  for(auto wordSum:multiWordOfOneArticle_) {
-    TRACE_VAR(wordSum.first,wordSum.second);
-    if(wordSum.second > 2) {
-      TRACE_VAR(wordSum.first,wordSum.second);
-      auto it = localMultiWordRank.find(wordSum.second);
-      if(it != localMultiWordRank.end()){
-        it->second.push_back(wordSum.first);
-      } else {
-        localMultiWordRank[wordSum.second] ={wordSum.first};
-      }
-    }
-  }
-  DUMP_VAR(localMultiWordRank.size());
-  string wordArrays;
-  for(auto &record:localMultiWordRank) {
-    TRACE_VAR(record.first);
-    auto words = record.second;
-    for(auto word:words) {
-      bool isShort= false;
-      for(const auto &word2:words){
-        auto found = word2.find(word);
-        if(found != string::npos && word2 != word){
-          TRACE_VAR(word);
-          TRACE_VAR(word2);
-          isShort= true;
-        }
-      }
-      if(isShort == false) {
-        wordArrays += "{";
-        wordArrays += word;
-        wordArrays += ",";
-        wordArrays += std::to_string(record.first);
-        wordArrays += "};";
-      }
-    }
-  }
-  wordArrays += "{}";
   upTask.put("word",wordArrays);
   string task_word_upPath = "/tmp/wai.native/task_word_up.json";
   pt::write_json(task_word_upPath,upTask);
