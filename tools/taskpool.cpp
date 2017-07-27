bf40dcd20651eb3549178fff2a9b13ad718d4b71
Wator Vapor <watorvapor@gmail.com>
2017-07-27 21:52:57 +0900

    Update taskpool.cpp

--- tools/taskpool.cpp
+++ tools/taskpool.cpp
@@ -4,9 +4,13 @@
 using namespace std;
 
 void taskpool_upd_main(void);
+void taskpool_collect(void);
+
 int main(int ac,char*av[])
 {
   std::thread t(taskpool_upd_main);
+  std::thread c(taskpool_collect);
   t.join();
+  c.join();
   return 0;
 }
