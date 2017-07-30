0245ce3d96829b9052ff241798c4a7d5faa97c96
Wator Vapor <watorvapor@gmail.com>
2017-07-30 21:26:18 +0900

    Update taskmerge.cpp

--- tools/taskmerge.cpp
+++ tools/taskmerge.cpp
@@ -4,9 +4,11 @@
 using namespace std;
 
 void taskmerge_upd_main(void);
-
+void taskmerge_write_main(void);
 int main(int ac, char *av[]) {
   std::thread t(taskmerge_upd_main);
+  std::thread w(taskmerge_write_main);
   t.join();
+  w.join();
   return 0;
 }
