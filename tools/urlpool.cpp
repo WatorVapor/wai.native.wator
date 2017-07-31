e08d8ca5cbce0e44f6c0ffb8a89558d7a2570348
Wator Vapor <watorvapor@gmail.com>
2017-07-31 22:22:43 +0900

    Update and rename taskpool.cpp to urlpool.cpp

--- tools/urlpool.cpp
+++ tools/urlpool.cpp
@@ -3,12 +3,12 @@
 #include <thread>
 using namespace std;
 
-void taskpool_upd_main(void);
-void taskpool_collect(void);
+void urlpool_upd_main(void);
+void urlpool_collect(void);
 
 int main(int ac, char *av[]) {
-  std::thread t(taskpool_upd_main);
-  std::thread c(taskpool_collect);
+  std::thread t(urlpool_upd_main);
+  std::thread c(urlpool_collect);
   t.join();
   c.join();
   return 0;
