8fcb3cee99d4363594a95024769b94212e8b9ca1
Wator Vapor <watorvapor@gmail.com>
2017-08-02 23:09:57 +0900

    Update ostrichword.db.cpp

--- watorword/ostrichword.db.cpp
+++ watorword/ostrichword.db.cpp
@@ -59,26 +59,28 @@ void OstrichWord::mergeWordByArticle(void) {
 }
 
 void OstrichWord::upWordByArticle2Master(const pt::ptree &task) {
-  auto wordArrays = pickupWordRanking();
   multiWordOfOneArticle_.clear();
 
-  pt::ptree upTask = task;
-  upTask.put("word", wordArrays);
-  string task_word_upPath = "/tmp/wai.native/task_word_up.json";
-  pt::write_json(task_word_upPath, upTask);
-  
-  auto tagOpt = task.get_optional<string>("tag");
-  if (tagOpt) {
-    auto tag = tagOpt.get();
-    string wgetTaskUp("curl -6 -F \"");
-    // string wgetTaskUp("curl -F \"");
-    wgetTaskUp += "file=@";
-    wgetTaskUp += task_word_upPath;
-    wgetTaskUp += "\" ";
-    wgetTaskUp += "\"https://www.wator.xyz/wai/text/train/ostrich/";
-    wgetTaskUp += tag;
-    wgetTaskUp += "\"";
-    DUMP_VAR(wgetTaskUp);
-    ::system(wgetTaskUp.c_str());
+  auto wordArrays = pickupWordRanking();
+  for(auto word:wordArrays) {
+    pt::ptree upTask = task;
+    upTask.put("word", word);
+    string task_word_upPath = "/tmp/wai.native/task_word_up.json";
+    pt::write_json(task_word_upPath, upTask);
+
+    auto tagOpt = task.get_optional<string>("tag");
+    if (tagOpt) {
+      auto tag = tagOpt.get();
+      string wgetTaskUp("curl -6 -F \"");
+      // string wgetTaskUp("curl -F \"");
+      wgetTaskUp += "file=@";
+      wgetTaskUp += task_word_upPath;
+      wgetTaskUp += "\" ";
+      wgetTaskUp += "\"https://www.wator.xyz/wai/text/train/ostrich/";
+      wgetTaskUp += tag;
+      wgetTaskUp += "\"";
+      DUMP_VAR(wgetTaskUp);
+      ::system(wgetTaskUp.c_str());
+    }
   }
 }
