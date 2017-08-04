5e7a9586b1114b6e0150cc557bd7b09b507984ac
Wator Vapor <watorvapor@gmail.com>
2017-08-05 08:50:48 +0900

    Update trainstorage.hpp

--- utilities/trainstorage.hpp
+++ utilities/trainstorage.hpp
@@ -13,8 +13,12 @@ private:
   void open(void);
   void close(void);
 private:
-  leveldb::DB *master_todo_db_;
-  leveldb::DB *ostrich_done_db_;
-  leveldb::DB *parrot_done_db_;
-  leveldb::DB *phoenix_done_db_;
+  leveldb::DB *master_todo_cn_db_;
+  leveldb::DB *master_todo_ja_db_;
+  leveldb::DB *ostrich_done_cn_db_;
+  leveldb::DB *parrot_done_cn_db_;
+  leveldb::DB *phoenix_done_cn_db_;
+  leveldb::DB *ostrich_done_ja_db_;
+  leveldb::DB *parrot_done_ja_db_;
+  leveldb::DB *phoenix_done_ja_db_;
 };
