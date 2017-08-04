67b677f406069163041db72ab238dfd92afd1de8
Wator Vapor <watorvapor@gmail.com>
2017-08-05 08:40:52 +0900

    Update trainstorage.hpp

--- utilities/trainstorage.hpp
+++ utilities/trainstorage.hpp
@@ -9,12 +9,10 @@ class TrainTaskStorage {
 public:
   TrainTaskStorage(const string &path, const string &prefix);
   ~TrainTaskStorage();
-  void write();
 private:
   void open();
   void close();
 private:
   string out_db_path_;
   string iter_db_path_;
-  leveldb::WriteBatch gSaveDBBatch;
 };
