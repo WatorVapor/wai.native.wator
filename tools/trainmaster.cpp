381b582b8d668223dc57bbd62cb529b870baf9cb
Wator Vapor <watorvapor@gmail.com>
2017-08-05 08:30:12 +0900

    Update trainmaster.cpp

--- tools/trainmaster.cpp
+++ tools/trainmaster.cpp
@@ -4,12 +4,9 @@
 using namespace std;
 
 void tain_master_upd_main(void);
-void tain_master_collect(void);
 
 int main(int ac, char *av[]) {
   std::thread t(tain_master_upd_main);
-  std::thread c(tain_master_collect);
   t.join();
-  c.join();
   return 0;
 }
