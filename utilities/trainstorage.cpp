35f8b8e23dfc7b2aba2b8ce634f551ff83993e5c
Wator Vapor <watorvapor@gmail.com>
2017-08-05 09:07:25 +0900

    Update trainstorage.cpp

--- utilities/trainstorage.cpp
+++ utilities/trainstorage.cpp
@@ -21,28 +21,11 @@ TrainTaskStorage::TrainTaskStorage() {
 
 TrainTaskStorage::~TrainTaskStorage() {}
 
-#define OPEN_LEVELDB_CREATE(name,ptr)\
+#define OPEN_LEVELDB(name,ptr,create)\
 {\
   if (name == nullptr) {\
     leveldb::Options options;\
-    options.create_if_missing = true;\
-    options.max_open_files = 512;\
-    options.paranoid_checks = true;\
-    options.compression = leveldb::kNoCompression;\
-    auto status = leveldb::DB::Open(options, name, &ptr);\
-    if (status.ok() == false) {\
-      DUMP_VAR(status.ToString());\
-      ptr = nullptr;\
-    }\
-  }\
-}
-  
-
-#define OPEN_LEVELDB_NO_CREATE(name,ptr)\
-{\
-  if (name == nullptr) {\
-    leveldb::Options options;\
-    options.create_if_missing = false;\
+    options.create_if_missing = create;\
     options.max_open_files = 512;\
     options.paranoid_checks = true;\
     options.compression = leveldb::kNoCompression;\
@@ -55,17 +38,16 @@ TrainTaskStorage::~TrainTaskStorage() {}
 }
 
 void TrainTaskStorage::open(void) {
-  OPEN_LEVELDB_NO_CREATE("/watorvapor/wai.storage/train/master/cn",master_todo_cn_db_);
-  OPEN_LEVELDB_NO_CREATE("/watorvapor/wai.storage/train/master/ja",master_todo_ja_db_);
-  OPEN_LEVELDB_CREATE("/watorvapor/wai.storage/train/ostrich/cn",ostrich_done_cn_db_);
-  OPEN_LEVELDB_CREATE("/watorvapor/wai.storage/train/parrot/cn",parrot_done_cn_db_);
-  OPEN_LEVELDB_CREATE("/watorvapor/wai.storage/train/phoenix/cn",phoenix_done_cn_db_);
-  OPEN_LEVELDB_CREATE("/watorvapor/wai.storage/train/ostrich/ja",ostrich_done_ja_db_);
-  OPEN_LEVELDB_CREATE("/watorvapor/wai.storage/train/parrot/ja",parrot_done_ja_db_);
-  OPEN_LEVELDB_CREATE("/watorvapor/wai.storage/train/phoenix/ja",phoenix_done_ja_db_);
+  OPEN_LEVELDB("/watorvapor/wai.storage/train/master/cn",master_todo_cn_db_,false);
+  OPEN_LEVELDB("/watorvapor/wai.storage/train/master/ja",master_todo_ja_db_,false);
+  
+  OPEN_LEVELDB("/watorvapor/wai.storage/train/ostrich/cn",ostrich_done_cn_db_,true);
+  OPEN_LEVELDB("/watorvapor/wai.storage/train/parrot/cn",parrot_done_cn_db_,true);
+  OPEN_LEVELDB("/watorvapor/wai.storage/train/phoenix/cn",phoenix_done_cn_db_,true);
+  OPEN_LEVELDB("/watorvapor/wai.storage/train/ostrich/ja",ostrich_done_ja_db_,true);
+  OPEN_LEVELDB("/watorvapor/wai.storage/train/parrot/ja",parrot_done_ja_db_,true);
+  OPEN_LEVELDB("/watorvapor/wai.storage/train/phoenix/ja",phoenix_done_ja_db_,true);
 }
-
-CLOSE_LEVELDB
 
 #define CLOSE_LEVELDB(ptr)\
 {\
