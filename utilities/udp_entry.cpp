0c2760ba968f4e254cf0db6ebc57dc84cb733dfc
Wator Vapor <watorvapor@gmail.com>
2017-08-14 22:20:44 +0900

    Update udp_entry.cpp

--- utilities/udp_entry.cpp
+++ utilities/udp_entry.cpp
@@ -1,61 +1,42 @@
-#include <cinttypes>
-#include <exception>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <thread>
-#include <chrono>
+#include "udp_entry.hpp"
+#include "log.hpp"
 using namespace std;
-
-#include <boost/array.hpp>
-#include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/exception/all.hpp>
-
-
-class udp_server {
 using namespace boost::asio::ip;
-public:
-  udp_server(shared_ptr<udp::socket> sock) : socket_(sock) { start_receive(); }
-  void send(const std::string &msg) {
-    boost::shared_ptr<std::string> message(new std::string(msg));
-    socket_->async_send_to(
-        boost::asio::buffer(*message), remote_endpoint_,
-        boost::bind(&udp_server::handle_send, this, message));
-  }
 
-private:
-  void start_receive() {
-    socket_->async_receive_from(
-        boost::asio::buffer(recv_buffer_), remote_endpoint_,
-        boost::bind(&udp_server::handle_receive, this,
-                    boost::asio::placeholders::error,
-                    boost::asio::placeholders::bytes_transferred));
-  }
+udp_server::udp_server(shared_ptr<udp::socket> sock) : socket_(sock) { start_receive(); }
+void udp_server::send(const std::string &msg) {
+  boost::shared_ptr<std::string> message(new std::string(msg));
+  socket_->async_send_to(
+      boost::asio::buffer(*message), remote_endpoint_,
+      boost::bind(&udp_server::handle_send, this, message));
+}
 
-  void handle_receive(const boost::system::error_code &error,
-                      std::size_t bytes_transferred) {
-    auto start = std::chrono::system_clock::now();
-    TRACE_VAR(remote_endpoint_);
-    TRACE_VAR(bytes_transferred);
-    std::string recv_str(recv_buffer_.data(), bytes_transferred);
-    TRACE_VAR(recv_str);
-    processText(recv_str);
-    if (!error || error == boost::asio::error::message_size) {
-      start_receive();
-    }
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double, std::milli> fp_ms = end - start;
-    DUMP_VAR(fp_ms.count());
-  }
+void udp_server::start_receive() {
+  socket_->async_receive_from(
+      boost::asio::buffer(recv_buffer_), remote_endpoint_,
+      boost::bind(&udp_server::handle_receive, this,
+                  boost::asio::placeholders::error,
+                  boost::asio::placeholders::bytes_transferred));
+}
 
-  void handle_send(boost::shared_ptr<std::string> msg) {
-    DUMP_VAR(msg);
-    DUMP_VAR(*msg);
+void udp_server::handle_receive(const boost::system::error_code &error,
+                    std::size_t bytes_transferred) {
+  auto start = std::chrono::system_clock::now();
+  TRACE_VAR(remote_endpoint_);
+  TRACE_VAR(bytes_transferred);
+  std::string recv_str(recv_buffer_.data(), bytes_transferred);
+  TRACE_VAR(recv_str);
+  processText(recv_str);
+  if (!error || error == boost::asio::error::message_size) {
+    start_receive();
   }
+  auto end = std::chrono::system_clock::now();
+  std::chrono::duration<double, std::milli> fp_ms = end - start;
+  DUMP_VAR(fp_ms.count());
+}
 
-  shared_ptr<udp::socket> socket_;
-  udp::endpoint remote_endpoint_;
-  boost::array<char, iConstMSGBufferMax> recv_buffer_;
-};
+void udp_server::handle_send(boost::shared_ptr<std::string> msg) {
+  DUMP_VAR(msg);
+  DUMP_VAR(*msg);
+}
 
