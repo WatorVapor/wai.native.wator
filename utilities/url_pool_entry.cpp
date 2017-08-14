a310a349f8933a0fb58c1f19ad73f97094cb06de
Wator Vapor <watorvapor@gmail.com>
2017-08-14 22:53:29 +0900

    Update url_pool_entry.cpp

--- utilities/url_pool_entry.cpp
+++ utilities/url_pool_entry.cpp
@@ -40,7 +40,8 @@ void urlpool_upd_main(void) {
       DUMP_VAR(port);
       savePort(port,"/watorvapor/wai.storage/conf/url.pool.api.json");
       auto server = std::make_shared<udp_server>(sock);
-      server->start_receive(processText);
+      procText = bind(processText);
+      server->start_receive(procText);
       DUMP_VAR(server.get());
       io_service->run();
     } catch (boost::exception &e) {
