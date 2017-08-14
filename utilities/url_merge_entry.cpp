d93e0f43f070b0351139c62b1b6179247d261a05
Wator Vapor <watorvapor@gmail.com>
2017-08-14 23:27:29 +0900

    Update url_merge_entry.cpp

--- utilities/url_merge_entry.cpp
+++ utilities/url_merge_entry.cpp
@@ -21,67 +21,15 @@ namespace pt = boost::property_tree;
 
 static const uint16_t iConstAPIPortRangeMin = 41284;
 static const uint16_t iConstAPIPortRangeMax = 41294;
-static const uint32_t iConstMSGBufferMax = 20*1024 * 1024;
 
 #include "log.hpp"
+#include "udp_entry.hpp"
 
-void processText(const string &text);
+static void processText(const std::string &text);
 
-class udp_server {
-public:
-  udp_server(shared_ptr<udp::socket> sock) : socket_(sock) { start_receive(); }
-  void send(const std::string &msg) {
-    boost::shared_ptr<std::string> message(new std::string(msg));
-    socket_->async_send_to(
-        boost::asio::buffer(*message), remote_endpoint_,
-        boost::bind(&udp_server::handle_send, this, message));
-  }
 
-private:
-  void start_receive() {
-    socket_->async_receive_from(
-        boost::asio::buffer(recv_buffer_), remote_endpoint_,
-        boost::bind(&udp_server::handle_receive, this,
-                    boost::asio::placeholders::error,
-                    boost::asio::placeholders::bytes_transferred));
-  }
 
-  void handle_receive(const boost::system::error_code &error,
-                      std::size_t bytes_transferred) {
-    auto start = std::chrono::system_clock::now();
-    TRACE_VAR(remote_endpoint_);
-    TRACE_VAR(bytes_transferred);
-    std::string recv_str(recv_buffer_.data(), bytes_transferred);
-    TRACE_VAR(recv_str);
-    processText(recv_str);
-    if (!error || error == boost::asio::error::message_size) {
-      start_receive();
-    }
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double, std::milli> fp_ms = end - start;
-    DUMP_VAR(fp_ms.count());
-  }
 
-  void handle_send(boost::shared_ptr<std::string> msg) {
-    DUMP_VAR(msg);
-    DUMP_VAR(*msg);
-  }
-
-  shared_ptr<udp::socket> socket_;
-  udp::endpoint remote_endpoint_;
-  boost::array<char, iConstMSGBufferMax> recv_buffer_;
-};
-
-static void savePort(uint16_t port) {
-  try {
-    pt::ptree portConf;
-    portConf.put("port", port);
-    pt::write_json("/watorvapor/wai.storage/conf/url.merge.api.json",
-                   portConf);
-  } catch (boost::exception &e) {
-    DUMP_VAR(boost::diagnostic_information(e));
-  }
-}
 
 static std::shared_ptr<udp_server> gUPDServer;
 void urlmerge_upd_main(void) {
@@ -93,8 +41,9 @@ void urlmerge_upd_main(void) {
           std::make_shared<udp::endpoint>(address::from_string("::1"), port);
       auto sock = std::make_shared<udp::socket>(*io_service, *ep);
       DUMP_VAR(port);
-      savePort(port);
+      savePort(port,"/watorvapor/wai.storage/conf/url.merge.api.json");
       gUPDServer = std::make_shared<udp_server>(sock);
+      gUPDServer->start_receive(processText);
       DUMP_VAR(gUPDServer.get());
       io_service->run();
     } catch (boost::exception &e) {
