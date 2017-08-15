662ee86a02dd21231e67d0b8510cb68e67dee593
Wator Vapor <watorvapor@gmail.com>
2017-08-15 17:24:55 +0900

    Update urlcrawler.cpp

--- tools/urlcrawler.cpp
+++ tools/urlcrawler.cpp
@@ -3,13 +3,19 @@
 #include <thread>
 using namespace std;
 
-void url_crawler_upd_main(void);
+void url_crawler_fetch_upd_main(void);
 void url_crawler_collect(void);
+void url_crawler_save_upd_main(void);
+void url_crawler_write_main(void);
 
 int main(int ac, char *av[]) {
   std::thread t(url_crawler_upd_main);
   std::thread c(url_crawler_collect);
+  std::thread s(url_crawler_save_upd_main);
+  std::thread w(url_crawler_write_main);
   t.join();
   c.join();
+  s.join();
+  w.join();
   return 0;
 }
