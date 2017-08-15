58ec965b10cbc3c89bdf83887a763c67ba2726d4
Wator Vapor <watorvapor@gmail.com>
2017-08-15 18:52:13 +0900

    Update log.hpp

--- watorword/log.hpp
+++ watorword/log.hpp
@@ -2,6 +2,9 @@
 #include <boost/log/core.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/trivial.hpp>
+#include <mutex>
+extern std::mutex gDumpVarMutex;
+
 #if 0
 #define DUMP_VAR(x)                                                            \
   {                                                                            \
@@ -11,21 +14,35 @@
 #endif
 
 #define DUMP_VAR(x)                                                            \
+{                                                                              \
+  std::lock_guard<std::mutex> lock(gDumpVarMutex);                             \
   std::cout << __func__ << ":" << __LINE__ << "::" << #x << "=<" << x << ">"   \
-            << std::endl;
+            << std::endl;                                                      \
+}
 #define DUMP_VAR2(x, y)                                                        \
+{                                                                              \
+  std::lock_guard<std::mutex> lock(gDumpVarMutex);                             \
   std::cout << __func__ << ":" << __LINE__ << "::" << #x << "," << #y << "=<"  \
-            << x << "," << y << ">" << std::endl;
+            << x << "," << y << ">" << std::endl;                              \
+}
 #define DUMP_VAR3(x, y, z)                                                     \
+{                                                                              \
+  std::lock_guard<std::mutex> lock(gDumpVarMutex);                             \
   std::cout << __func__ << ":" << __LINE__ << "::" << #x << "," << #y << ","   \
-            << #z << "=<" << x << "," << y << "," << z << ">" << std::endl;
+            << #z << "=<" << x << "," << y << "," << z << ">" << std::endl;    \
+}
 #define DUMP_VAR4(x, y, z, a)                                                  \
+{                                                                              \
+  std::lock_guard<std::mutex> lock(gDumpVarMutex);                             \
   std::cout << __func__ << ":" << __LINE__ << "::" << #x << "," << #y << ","   \
             << #z << "," << #a << "=<" << x << "," << y << "," << z << ","     \
-            << a << ">" << std::endl;
+            << a << ">" << std::endl;                                          \
+}
 #define DUMP_VAR5(x, y, z, a, b)                                               \
+{                                                                              \
+  std::lock_guard<std::mutex> lock(gDumpVarMutex);                             \
   std::cout << __func__ << ":" << __LINE__ << "::" << #x << "," << #y << ","   \
             << #z << "," << #a << "," << #b << "=<" << x << "," << y << ","    \
-            << z << "," << a << "," << b << ">" << std::endl;
-
+            << z << "," << a << "," << b << ">" << std::endl;                  \
+}
 #define TRACE_VAR(...)
