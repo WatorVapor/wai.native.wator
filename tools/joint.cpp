bbd8979f401ad14ad2e0f49233e7baca8644fa38
Wator Vapor <watorvapor@gmail.com>
2017-08-16 21:23:15 +0900

    Update joint.cpp

--- tools/joint.cpp
+++ tools/joint.cpp
@@ -13,25 +13,35 @@ std::shared_ptr<URLStorage> gJAMasterStorage;
 std::shared_ptr<URLStorage> gJATodoStorage;
 
 
+std::shared_ptr<URLStorage> gCNJointStorage;
+std::shared_ptr<URLStorage> gJAJointStorage;
 
 
 int main(int ac, char *av[]) {
   
   gCNMasterStorage = std::make_shared<URLStorage>("/watorvapor/wai.storage/leveldb/cn/master");
   gCNTodoStorage = std::make_shared<URLStorage>("/watorvapor/wai.storage/leveldb/cn/todo");
+  gCNJointStorage = std::make_shared<URLStorage>("/watorvapor/wai.storage/leveldb/cn/joint");
+
   gJAMasterStorage = std::make_shared<URLStorage>("/watorvapor/wai.storage/leveldb/ja/master");
   gJATodoStorage = std::make_shared<URLStorage>("/watorvapor/wai.storage/leveldb/ja/todo");
+  gJAJointStorage = std::make_shared<URLStorage>("/watorvapor/wai.storage/leveldb/ja/joint");
   
   gCNMasterStorage->openDB();
   gCNTodoStorage->openDB();
+  gCNJointStorage->openDB();
+  
   gJAMasterStorage->openDB();
   gJATodoStorage->openDB();
-  
+  gJAJointStorage->openDB();  
   
   gCNMasterStorage->closeDB();
   gCNTodoStorage->closeDB();
+  gCNJointStorage->closeDB();
+  
   gJAMasterStorage->closeDB();
   gJATodoStorage->closeDB();
+  gJAJointStorage->closeDB();
   
   return 0;
 }
