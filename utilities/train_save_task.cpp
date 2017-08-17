12adda52a9ac15f5d0fab2daf194b2e3d62dceac
Wator Vapor <watorvapor@gmail.com>
2017-08-18 00:21:37 +0900

    Update train_save_task.cpp

--- utilities/train_save_task.cpp
+++ utilities/train_save_task.cpp
@@ -28,14 +28,36 @@ namespace pt = boost::property_tree;
 #include "udp_entry.hpp"
 extern std::shared_ptr<udp_server> gSaveServer;
 
+string sha1(const string &data);
 
-void saveOstrichTask(const string &lang) {
+
+static void markOstrich(const string &url,const string &lang) {
+  auto start = std::chrono::system_clock::now();
+  auto doneName = sha1(url);
+  DUMP_VAR2(doneName, url);
+  if(lang == "cn") {
+    gCNDoneOstrichStorage->add(doneName,url);
+    gCNTodoOstrichStorage->remove(doneName);
+  } else if(lang == "ja") {
+    gJADoneOstrichStorage->add(doneName,url);
+    gJATodoOstrichStorage->remove(doneName);
+  } else {
+  }
+  auto end = std::chrono::system_clock::now();
+  std::chrono::duration<double, std::milli> markOstrich_ms = end - start;
+  DUMP_VAR(markOstrich_ms.count());
+}
+
+
+
+void saveOstrichTask(const string &lang,const string &url,const string &word) {
+  //markOstrich(url,lang);
   gSaveServer->send("success");
 }
-void saveParrotTask(const string &lang) {
+void saveParrotTask(const string &lang,const string &url,const string &word) {
   gSaveServer->send("success");
 }
-void savePhoenixTask(const string &lang) {
+void savePhoenixTask(const string &lang,const string &url,const string &word) {
   gSaveServer->send("success");
 }
 
