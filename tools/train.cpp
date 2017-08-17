0187d2230703dbff8c3a7660362b3a957afc54e2
Wator Vapor <watorvapor@gmail.com>
2017-08-17 22:50:08 +0900

    Update train.cpp

--- tools/train.cpp
+++ tools/train.cpp
@@ -6,12 +6,16 @@ using namespace std;
 void train_fetch_upd_main(void);
 void train_collect(void);
 void train_save_upd_main(void);
-void train_save(void);
+void train_word_write(void);
 
 int main(int ac, char *av[]) {
   std::thread t(tain_fetch_upd_main);
   std::thread c(train_collect);
+  std::thread s(train_save_upd_main);
+  std::thread w(train_word_write);
   t.join();
   c.join();
+  s.join();
+  w.join();
   return 0;
 }
