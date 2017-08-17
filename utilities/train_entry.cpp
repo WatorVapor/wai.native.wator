2831bb1a585e43edc7fdce5c94574ca8ecb69721
Wator Vapor <watorvapor@gmail.com>
2017-08-17 20:40:27 +0900

    Update train_entry.cpp

--- utilities/train_entry.cpp
+++ utilities/train_entry.cpp
@@ -92,42 +92,5 @@ void processText(const std::string &text) {
   }
 }
 
-#include <boost/filesystem.hpp>
-namespace fs = boost::filesystem;
-#include <condition_variable>
-#include <mutex>
-
-static vector<string> gVectTodoPathCN;
-static vector<string> gVectTodoPathJA;
-static std::mutex gVectoPathMutex;
-static std::condition_variable gVectoPathCV;
-static std::mutex gVectoPathCvMutex;
-
-static int iConstPathCacheMax = 32;
-
-string fetchOstrichTask(const string &lang) {
-  DUMP_VAR(lang);
-  if (lang == "cn") {
-  } else if (lang == "ja") {
-  } else {
-  }
-  return "";
-}
-string fetchParrotTask(const string &lang) {
-  DUMP_VAR(lang);
-  if (lang == "cn") {
-  } else if (lang == "ja") {
-  } else {
-  }
-  return "";
-}
-string fetchPhoenixTask(const string &lang) {
-  DUMP_VAR(lang);
-  if (lang == "cn") {
-  } else if (lang == "ja") {
-  } else {
-  }
-  return "";
-}
 
 
