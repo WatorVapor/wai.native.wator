8888597e4669ecd33c0bbf83a7c2c05a9b9dd67b
Wator Vapor <watorvapor@gmail.com>
2017-08-17 22:54:31 +0900

    Update train_save_task.cpp

--- utilities/train_save_task.cpp
+++ utilities/train_save_task.cpp
@@ -1,3 +1,29 @@
+#include <cinttypes>
+#include <exception>
+#include <iostream>
+#include <iomanip>
+#include <memory>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <list>
+using namespace std;
+
+
+#include <condition_variable>
+#include <mutex>
+#include <atomic>
+
+#include "log.hpp"
+
+
+#include <boost/foreach.hpp>
+#include <boost/optional.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+namespace pt = boost::property_tree;
+#include <boost/algorithm/string.hpp>
+
 string saveOstrichTask(const string &lang) {
 }
 string saveParrotTask(const string &lang) {
