0aae20275ef2a2fdfcea1196f077e4baa632f7fa
Wator Vapor <watorvapor@gmail.com>
2017-08-17 22:52:02 +0900

    Update train_save_task.cpp

--- utilities/train_save_task.cpp
+++ utilities/train_save_task.cpp
@@ -1,3 +1,10 @@
+string saveOstrichTask(const string &lang) {
+}
+string saveParrotTask(const string &lang) {
+}
+string savePhoenixTask(const string &lang) {
+}
+
 void train_word_write(void) {
 }
 
