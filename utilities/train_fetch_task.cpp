4f011d19bea409c5cc400e45f18abe4bca96fd29
Wator Vapor <watorvapor@gmail.com>
2017-08-17 20:40:24 +0900

    Update train_fetch_task.cpp

--- utilities/train_fetch_task.cpp
+++ utilities/train_fetch_task.cpp
@@ -1,1 +1,38 @@
 
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+#include <condition_variable>
+#include <mutex>
+
+static vector<string> gVectTodoPathCN;
+static vector<string> gVectTodoPathJA;
+static std::mutex gVectoPathMutex;
+static std::condition_variable gVectoPathCV;
+static std::mutex gVectoPathCvMutex;
+
+static int iConstPathCacheMax = 32;
+
+string fetchOstrichTask(const string &lang) {
+  DUMP_VAR(lang);
+  if (lang == "cn") {
+  } else if (lang == "ja") {
+  } else {
+  }
+  return "";
+}
+string fetchParrotTask(const string &lang) {
+  DUMP_VAR(lang);
+  if (lang == "cn") {
+  } else if (lang == "ja") {
+  } else {
+  }
+  return "";
+}
+string fetchPhoenixTask(const string &lang) {
+  DUMP_VAR(lang);
+  if (lang == "cn") {
+  } else if (lang == "ja") {
+  } else {
+  }
+  return "";
+}
