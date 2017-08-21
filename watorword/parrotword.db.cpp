3ff7965ba3ae45f757bddf5e52484d1f0f9b5f9a
Wator Vapor <watorvapor@gmail.com>
2017-08-21 23:39:08 +0900

    Update parrotword.db.cpp

--- watorword/parrotword.db.cpp
+++ watorword/parrotword.db.cpp
@@ -28,3 +28,35 @@ bool ParrotWord::loadMaster(bool forceCast) {
 
 void unloadMasterFromDB(void);
 void ParrotWord::unloadMaster(void) { unloadMasterFromDB(); }
+
+
+void ParrotWord::dumpRank() {
+  for (auto rPair : statisticsRank_) {
+    auto word = rPair.first;
+    auto weight = std::get<0>(rPair.second);
+    auto weight_orig = std::get<1>(rPair.second);
+    DUMP_VAR4(word, word.size(), weight, weight_orig);
+  }
+}
+
+
+void ParrotWord::dumpSeq() {
+  for (auto elem : wordSeq_) {
+    TRACE_VAR(elem.first);
+    auto word = std::get<0>(elem.second);
+    auto pos = std::get<1>(elem.second);
+    auto range = std::get<2>(elem.second);
+    auto weight = std::get<3>(elem.second);
+    DUMP_VAR4(word, pos, range, weight);
+  }
+}
+void ParrotWord::dumpPreds() {
+  for (auto elem : wordSeqTopSelected_) {
+    TRACE_VAR(elem.first);
+    auto word = std::get<0>(elem.second);
+    auto pos = std::get<1>(elem.second);
+    auto range = std::get<2>(elem.second);
+    auto weight = std::get<3>(elem.second);
+    DUMP_VAR4(word, pos, range, weight);
+  }
+}
