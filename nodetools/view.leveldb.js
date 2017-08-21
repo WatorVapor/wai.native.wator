f51012680f5e620f15ac8c53b13e1a05a7711962
Wator Vapor <watorvapor@gmail.com>
2017-08-21 21:55:05 +0900

    Update view.leveldb.js

--- nodetools/view.leveldb.js
+++ nodetools/view.leveldb.js
@@ -1,2 +1,16 @@
 var levelup = require('levelup')
 var db = levelup('/watorvapor/wai.storage/train/ostrich/cp.dict/cn')
+db.createReadStream()
+  .on('data', function (data) {
+    console.log(data.key, '=', data.value)
+  })
+  .on('error', function (err) {
+    console.log('Oh my!', err)
+  })
+  .on('close', function () {
+    console.log('Stream closed')
+  })
+  .on('end', function () {
+    console.log('Stream ended')
+  })
+
