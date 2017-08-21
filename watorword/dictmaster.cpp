a0c661980e30e1676b0532860b336a889113460a
Wator Vapor <watorvapor@gmail.com>
2017-08-21 22:32:37 +0900

    Update and rename dictstorage.cpp to dictmaster.cpp

--- watorword/dictmaster.cpp
+++ watorword/dictmaster.cpp
@@ -11,26 +11,9 @@ using namespace std;
 
 #include <boost/format.hpp>
 
-#include "dictstorage.hpp"
+#include "dictmaster.hpp"
+#include "log.hpp"
 
-#define DUMP_VAR(x)                                                          \
-  std::cout << __func__ << ":" << __LINE__ << "::" << #x << "=<" << x << ">" \
-            << std::endl;
-#define DUMP_VAR2(x, y)                                                       \
-  std::cout << __func__ << ":" << __LINE__ << "::" << #x << "," << #y << "=<" \
-            << x << "," << y << ">" << std::endl;
-#define DUMP_VAR3(x, y, z)                                                   \
-  std::cout << __func__ << ":" << __LINE__ << "::" << #x << "," << #y << "," \
-            << #z << "=<" << x << "," << y << "," << z << ">" << std::endl;
-#define DUMP_VAR4(x, y, z, a)                                                \
-  std::cout << __func__ << ":" << __LINE__ << "::" << #x << "," << #y << "," \
-            << #z << "," << #a << "=<" << x << "," << y << "," << z << ","   \
-            << a << ">" << std::endl;
-#define DUMP_VAR5(x, y, z, a, b)                                             \
-  std::cout << __func__ << ":" << __LINE__ << "::" << #x << "," << #y << "," \
-            << #z << "," << #a << "," << #b << "=<" << x << "," << y << ","  \
-            << z << "," << a << "," << b << ">" << std::endl;
-#define TRACE_VAR(x)
 
 static leveldb::DB *gSaveDB = nullptr;
 
