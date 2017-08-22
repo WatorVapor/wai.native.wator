d2b8dbaaea2b9fcd0a38d6957b8dca514eb12d5c
Wator Vapor <watorvapor@gmail.com>
2017-08-22 22:31:16 +0900

    Update parrotword.db.cpp

--- watorword/parrotword.db.cpp
+++ watorword/parrotword.db.cpp
@@ -62,7 +62,13 @@ void ParrotWord::dumpPreds() {
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_utility.hpp>
 #include <boost/graph/graphviz.hpp>
-typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS> Graph;
+
+#include <boost/graph/directed_graph.hpp>
+#include <boost/graph/labeled_graph.hpp>
+
+//typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS> Graph;
+typedef boost::labeled_graph<Digraph, string> Graph;
+
 typedef std::pair<string, string> Edge;
 typedef boost::graph_traits < Graph >::vertex_descriptor Vertex;
 
@@ -70,7 +76,7 @@ void ParrotWord::dumpDot(void) {
   const Graph g;
   for (auto elem : wordHintSeq_) {
     auto word = std::get<0>(elem.second);
-    Vertex v1 = boost::add_vertex(string(word), g);
+    boost::add_vertex(word, g);
   }
   boost::write_graphviz(std::cout, g);
 }
