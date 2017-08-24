3e32dc882a2161c913dfa5f0fb33d2aa632364e9
Wator Vapor <watorvapor@gmail.com>
2017-08-24 10:10:03 +0900

    Update parrotword.cpp

--- watorword/parrotword.cpp
+++ watorword/parrotword.cpp
@@ -52,21 +52,7 @@ void ParrotWord::dumpPredWords() {
   }
   std::cout << std::endl;
 }
-void ParrotWord::mergeWordPrediction(void) {
-  for (auto parrot : prediWords_) {
-    auto it = gMultiWordSum.find(parrot);
-    if (it != gMultiWordSum.end()) {
-      it->second++;
-    } else {
-      gMultiWordSum[parrot] = 1;
-    }
-  }
-}
 
-void ParrotWord::commitArticle(void) {
-  gMultiWordSum.clear();
-  ap_ = 0;
-}
 
 void ParrotWord::getRawRank(const vector<string> &Bytes) {
   statisticsRank_.clear();
