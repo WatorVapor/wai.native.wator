d68d26133d7cba4997d5439e492098a173d02b59
Wator Vapor <watorvapor@gmail.com>
2017-08-25 23:05:50 +0900

    Update ostrichword.db.cpp

--- watorword/ostrichword.db.cpp
+++ watorword/ostrichword.db.cpp
@@ -14,14 +14,14 @@ using namespace std;
 #include "log.hpp"
 #include "ostrichword.hpp"
 
-void OstrichWord::upWordByArticle2Master(const pt::ptree &task) {
+void OstrichWord::commitArticle(const pt::ptree &task,const string &ws) {
   auto wordArrays = pickupWordRanking();
   multiWordOfOneArticle_.clear();
   for (auto word : wordArrays) {
     DUMP_VAR(word);
     pt::ptree upTask = task;
     upTask.put("word", word);
-    string task_word_upPath = "/tmp/wai.native/task_word_up.json";
+    string task_word_upPath = ws + "/task_word_up.json";
     pt::write_json(task_word_upPath, upTask);
 
     auto tagOpt = task.get_optional<string>("tag");
