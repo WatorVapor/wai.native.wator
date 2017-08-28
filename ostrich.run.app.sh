16bf617c5809b3b62387d420ee23b54112c01360
Wator Vapor <watorvapor@gmail.com>
2017-08-29 06:24:55 +0900

    Update ostrich.run.app.sh

--- ostrich.run.app.sh
+++ ostrich.run.app.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ulimit -c unlimited
-nohup object/wator.parrot.app
+nohup object/wator.ostrich.app >wator.ostrich.app.1.log &
+nohup object/wator.ostrich.app >wator.ostrich.app.2.log &
+nohup object/wator.ostrich.app >wator.ostrich.app.3.log &
