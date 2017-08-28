9be47b60176a6cefe1c5b13b948cd18eb45b97e0
Wator Vapor <watorvapor@gmail.com>
2017-08-28 23:15:27 +0900

    Update dictmaster.cpp

--- watorword/dictmaster.cpp
+++ watorword/dictmaster.cpp
@@ -14,13 +14,10 @@ using namespace std;
 #include "dictmaster.hpp"
 #include "log.hpp"
 
-static leveldb::DB *gSaveDB = nullptr;
 
-DictionaryMaster::DictionaryMaster(const string &path, const string &prefix) {
-  out_db_path_ = path + "/" + prefix;
-  DUMP_VAR(out_db_path_);
-  iter_db_path_ = path + "/snapshot/" + prefix + "_iter.";
-  DUMP_VAR(iter_db_path_);
+DictionaryMaster::DictionaryMaster(const string &path) {
+  master_db_path_ = path ;
+  master_cast_db_path_ = path + ".cast";
 }
 
 DictionaryMaster::~DictionaryMaster() {}
@@ -48,57 +45,7 @@ void DictionaryMaster::closeDB() {
     gSaveDB = nullptr;
   }
 }
-void DictionaryMaster::writeDB() {
-  if (gSaveDB != nullptr) {
-    leveldb::WriteOptions writeOptions;
-    writeOptions.sync = true;
-    auto status = gSaveDB->Write(writeOptions, &gSaveDBBatch);
-    DUMP_VAR(status.ToString());
-    if (status.ok()) {
-      gSaveDBBatch.Clear();
-      dumpSnapshotDB();
-    }
-  }
-}
 
-static int iConstSnapshotCounter = 100;
-void DictionaryMaster::dumpSnapshotDB() {
-  static int iCounter = 0;
-  if (iCounter++ % iConstSnapshotCounter != iConstSnapshotCounter - 1) {
-    return;
-  }
-  static int iSnapshotNumber = 0;
-  string pathIter = (boost::format("%08d") % iSnapshotNumber++).str();
-  // std::reverse(pathIter.begin(), pathIter.end());
-  // string pathDump = "./db/baidu.baike/snapshot/word_ostrich_iter." +
-  // pathIter;
-  string pathDump = iter_db_path_ + pathIter;
-  leveldb::DB *dumpdb = nullptr;
-  leveldb::Options options;
-  options.create_if_missing = true;
-  options.compression = leveldb::kNoCompression;
-  auto status = leveldb::DB::Open(options, pathDump, &dumpdb);
-  if (status.ok()) {
-    leveldb::WriteBatch batch;
-    if (gSaveDB) {
-      leveldb::ReadOptions readOptions;
-      readOptions.snapshot = gSaveDB->GetSnapshot();
-      auto it = gSaveDB->NewIterator(readOptions);
-      it->SeekToFirst();
-      DUMP_VAR(it->Valid());
-      while (it->Valid()) {
-        batch.Put(it->key(), it->value());
-        it->Next();
-      }
-      delete it;
-      gSaveDB->ReleaseSnapshot(readOptions.snapshot);
-    }
-    leveldb::WriteOptions writeOptions;
-    writeOptions.sync = true;
-    auto status = dumpdb->Write(writeOptions, &batch);
-    delete dumpdb;
-  }
-}
 
 void DictionaryMaster::putWord(const string &word, int counter) {
   leveldb::ReadOptions readOptions;
