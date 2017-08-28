2ecb3bad37dd406eceb27c2f74ddf1484d839787
Wator Vapor <watorvapor@gmail.com>
2017-08-29 06:34:58 +0900

    Update parrot.run.app.sh

--- parrot.run.app.sh
+++ parrot.run.app.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ulimit -c unlimited
-nohup object/wator.parrot.app
+nohup object/wator.parrot.app >wator.parrot.app.1.log &
+nohup object/wator.parrot.app >wator.parrot.app.2.log &
+nohup object/wator.parrot.app >wator.parrot.app.3.log &
