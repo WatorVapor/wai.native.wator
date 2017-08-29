752ba7556f8472ae9892443c349ca7de58ac6715
Wator Vapor <watorvapor@gmail.com>
2017-08-29 23:34:58 +0900

    Update dictstorage.cpp

--- utilities/dictstorage.cpp
+++ utilities/dictstorage.cpp
@@ -26,7 +26,7 @@ void DictionaryStorage::openDB() {
     options.paranoid_checks = true;
     options.compression = leveldb::kNoCompression;
     auto status = leveldb::DB::Open(options, db_path_, &save_);
-    DUMP_VAR(status.ToString());
+    DUMP_VAR2(db_path_,status.ToString());
     if (status.ok() == false) {
       save_ = nullptr;
     }
