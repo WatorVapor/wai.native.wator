c625d38cfc250321ec7b9c3777075e24025adde0
Wator Vapor <watorvapor@gmail.com>
2017-08-31 22:11:01 +0900

    Update phoenixword.db.cpp

--- watorword/phoenixword.db.cpp
+++ watorword/phoenixword.db.cpp
@@ -99,3 +99,79 @@ void PhoenixWord::dumpPredWords() {
   }
   std::cout << std::endl;
 }
+
+
+
+#include <boost/graph/directed_graph.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <sstream>
+#include <string>
+#include <utility>
+using namespace boost;
+using namespace boost::graph_detail;
+
+#include <boost/algorithm/string.hpp>
+
+typedef boost::directed_graph<> Graph;
+
+typedef std::pair<string, string> Edge;
+typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
+
+void PhoenixWord::dumpDot(void) {
+  Graph g;
+  multimap<int, std::tuple<string, Vertex>> vertexs;
+  static vector<string> labelVertex;
+  for (auto elem : wordHintSeq_) {
+    auto word = std::get<0>(elem.second);
+    auto position = std::get<1>(elem.second);
+    auto vrtx = g.add_vertex();
+    auto vrtxPr = std::make_tuple(word, vrtx);
+    vertexs.insert(std::make_pair(position, vrtxPr));
+    labelVertex.push_back(word);
+  }
+  for (auto elem : wordHintSeq_) {
+    auto word = std::get<0>(elem.second);
+    auto position = std::get<1>(elem.second);
+    auto range = std::get<2>(elem.second);
+    auto next = position + range;
+    auto rangeSelf = vertexs.equal_range(position);
+    for (auto itSelf = rangeSelf.first; itSelf != rangeSelf.second; itSelf++) {
+      auto wordSelf = std::get<0>(itSelf->second);
+      auto vrtxSelf = std::get<1>(itSelf->second);
+      if (word == wordSelf) {
+        auto rangeNext = vertexs.equal_range(next);
+        for (auto itNext = rangeNext.first; itNext != rangeNext.second;
+             itNext++) {
+          auto vrtxNext = std::get<1>(itNext->second);
+          g.add_edge(vrtxSelf, vrtxNext);
+        }
+        break;
+      }
+    }
+  }
+  static int counter = 0;
+  struct sample_graph_writer {
+    void operator()(std::ostream& out, void* data) const {
+      auto word = labelVertex.at(counter);
+      out << " [ label = \"";
+      out << word;
+      out << "\" ]";
+      counter++;
+    }
+  };
+  sample_graph_writer gw;
+
+  std::stringstream ss;
+  boost::write_graphviz(ss, g, gw);
+  auto dotStr = ss.str();
+  boost::algorithm::replace_all(
+      dotStr, "digraph G {",
+      "digraph G { \n rankdir=LR;\n graph [charset=\"UTF-8\"];\n");
+  DUMP_VAR(dotStr);
+  // dot -v -T svg 1.dot -o 1.svg
+
+  counter = 0;
+  labelVertex.clear();
+}
+
