9748456d2e09c1eab8cc74f8d36d5c27d9228b9a
Wator Vapor <watorvapor@gmail.com>
2017-09-02 13:15:23 +0900

    Update phoenixword.graph.cpp

--- watorword/phoenixword.graph.cpp
+++ watorword/phoenixword.graph.cpp
@@ -145,7 +145,7 @@ void PhoenixWord::calcPrediction(const multimap<int, WordElement> &confuse) {
   boost::dijkstra_shortest_paths(g,vrtxStart,predmap);
   
   
-  graph_traits < Graph >::vertex_iterator vi, vend;
+  boost::graph_traits < Graph >::vertex_iterator vi, vend;
   for (boost::tie(vi, vend) = vertices(g); vi != vend; ++vi) {
     std::cout << "distance(" << labelVertex.at(*vi) << ") = " << distance[*vi] << ", ";
     std::cout << "parent(" << labelVertex.at(*vi) << ") = " << labelVertex.at(parents[*vi]) << std::endl;
