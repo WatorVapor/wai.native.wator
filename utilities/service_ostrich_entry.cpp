a8b2ed6d9eabb3b93604e12946243cf7ba6cae94
Wator Vapor <watorvapor@gmail.com>
2017-09-05 20:58:24 +0900

    Update service_ostrich_entry.cpp

--- utilities/service_ostrich_entry.cpp
+++ utilities/service_ostrich_entry.cpp
@@ -22,21 +22,21 @@ namespace pt = boost::property_tree;
 #include "log.hpp"
 #include "udp_entry.hpp"
 
-static const uint16_t iConstFetchAPIPortRangeMin = 41284;
-static const uint16_t iConstFetchAPIPortRangeMax = 41294;
+static const uint16_t iConstOstrichFetchAPIPortRangeMin = 41284;
+static const uint16_t iConstOstrichFetchAPIPortRangeMax = 41294;
 static void processText(const std::string &text);
 std::shared_ptr<udp_server> gFetchTrainServer;
 
 void train_ostrich_fetch_upd_main(void) {
   auto io_service = std::make_shared<boost::asio::io_service>();
-  for (uint16_t port = iConstFetchAPIPortRangeMin;
-       port < iConstFetchAPIPortRangeMax; port++) {
+  for (uint16_t port = iConstOstrichFetchAPIPortRangeMin;
+       port < iConstOstrichFetchAPIPortRangeMax; port++) {
     try {
       auto ep =
           std::make_shared<udp::endpoint>(address::from_string("::1"), port);
       auto sock = std::make_shared<udp::socket>(*io_service, *ep);
       DUMP_VAR(port);
-      savePort(port, "/watorvapor/wai.storage/conf/train.fetch.api.json");
+      savePort(port, "/watorvapor/wai.storage/conf/train.ostrich.fetch.api.json");
       gFetchTrainServer = std::make_shared<udp_server>(sock);
       gFetchTrainServer->start_receive(processText);
       DUMP_VAR(gFetchTrainServer.get());
