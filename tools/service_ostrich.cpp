d60c2eb7c56c11573c3138591894232f05a29248
Wator Vapor <watorvapor@gmail.com>
2017-09-05 21:23:42 +0900

    Update service_ostrich.cpp

--- tools/service_ostrich.cpp
+++ tools/service_ostrich.cpp
@@ -6,16 +6,13 @@ using namespace std;
 void train_ostrich_fetch_upd_main(void);
 void train_ostrich_collect(void);
 void train_ostrich_save_upd_main(void);
-void train_ostrich_word_write(void);
 
 int main(int ac, char *av[]) {
   std::thread t(train_ostrich_fetch_upd_main);
   std::thread c(train_ostrich_collect);
   std::thread s(train_ostrich_save_upd_main);
-  std::thread w(train_ostrich_word_write);
   t.join();
   c.join();
   s.join();
-  w.join();
   return 0;
 }
