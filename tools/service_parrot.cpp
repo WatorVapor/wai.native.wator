03c57aaf7e33415ceb16bf0d73a5d6ea1e13216e
Wator Vapor <watorvapor@gmail.com>
2017-09-05 21:23:01 +0900

    Update service_parrot.cpp

--- tools/service_parrot.cpp
+++ tools/service_parrot.cpp
@@ -6,16 +6,13 @@ using namespace std;
 void train_parrot_fetch_upd_main(void);
 void train_parrot_collect(void);
 void train_parrot_save_upd_main(void);
-void train_parrot_word_write(void);
 
 int main(int ac, char *av[]) {
   std::thread t(train_parrot_fetch_upd_main);
   std::thread c(train_parrot_collect);
   std::thread s(train_parrot_save_upd_main);
-  std::thread w(train_parrot_word_write);
   t.join();
   c.join();
   s.join();
-  w.join();
   return 0;
 }
