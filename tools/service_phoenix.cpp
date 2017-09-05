7a5ce3462cb1951c9ad9427070d3f79a9ca1ea6f
Wator Vapor <watorvapor@gmail.com>
2017-09-05 21:24:41 +0900

    Update service_phoenix.cpp

--- tools/service_phoenix.cpp
+++ tools/service_phoenix.cpp
@@ -6,16 +6,13 @@ using namespace std;
 void train_phoenix_fetch_upd_main(void);
 void train_phoenix_collect(void);
 void train_phoenix_save_upd_main(void);
-void train_phoenix_word_write(void);
 
 int main(int ac, char *av[]) {
   std::thread t(train_phoenix_fetch_upd_main);
   std::thread c(train_phoenix_collect);
   std::thread s(train_phoenix_save_upd_main);
-  std::thread w(train_phoenix_word_write);
   t.join();
   c.join();
   s.join();
-  w.join();
   return 0;
 }
