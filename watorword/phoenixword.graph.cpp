9be6e95727c4f87eda507c517da8a6910dda4c07
Wator Vapor <watorvapor@gmail.com>
2017-09-19 22:26:49 +0900

    Update phoenixword.graph.cpp

--- watorword/phoenixword.graph.cpp
+++ watorword/phoenixword.graph.cpp
@@ -189,6 +189,108 @@ void PhoenixWord::calcPrediction(const multimap<int, WordElement> &confuse) {
 }
 
 string PhoenixWord::createGraph(void) {
+  Graph g;
+  multimap<int, std::tuple<string, Vertex,double,double>> vertexWator;
+  vector<std::tuple<string,double,double>> labelVertex;
+  
+  auto vrtxStart = boost::add_vertex(g);
+  auto vrtxPrStart = std::make_tuple("S", vrtxStart,1.0,1.0);
+  vertexWator.insert(std::make_pair(-1, vrtxPrStart));
+  labelVertex.push_back(std::make_tuple("S",1.0,1.0));
+  
+  int posLast = 0;
+  for (auto elem : wordAdjustedSeq_) {
+    auto word = std::get<0>(elem.second);
+    auto position = std::get<1>(elem.second);
+    auto weight = std::get<3>(elem.second);
+    auto weightO = std::get<4>(elem.second);
+    auto vrtx = boost::add_vertex(g);
+    auto vrtxPr = std::make_tuple(word, vrtx,weight,weightO);
+    vertexWator.insert(std::make_pair(position, vrtxPr));
+    labelVertex.push_back(std::make_tuple(word,weight,weightO));
+    posLast = position + word.size();
+  }
+  auto vrtxEnd = add_vertex(g);
+  auto vrtxPrvrtxEnd = std::make_tuple("E", vrtxEnd,1.0,1.0);
+  vertexWator.insert(std::make_pair(posLast,vrtxPrvrtxEnd));
+  labelVertex.push_back(std::make_tuple("E",1.0,1.0));
+ 
+  // add dummy start.
+  {
+    auto startPos = wordAdjustedSeq_.begin()->first;
+    auto rangeSelf = vertexWator.equal_range(startPos);
+    for (auto itSelf = rangeSelf.first; itSelf != rangeSelf.second; itSelf++) {
+      auto wordSelf = std::get<0>(itSelf->second);
+      auto vrtxSelf = std::get<1>(itSelf->second);
+      auto weightR = std::get<2>(itSelf->second);
+      auto weightRO = std::get<3>(itSelf->second);
+      auto ed = boost::add_edge(vrtxStart, vrtxSelf,g);
+      auto weightOld = boost::get(boost::edge_weight_t(), g, ed.first);
+      boost::put(boost::edge_weight_t(), g, ed.first, 1.0/weightR);
+      auto weightNew = boost::get(boost::edge_weight_t(), g, ed.first);
+      TRACE_VAR(weightR,weightRO,weightOld,weightNew);
+    }
+  }
+  
+
+  
+  
+  
+  for (auto elem : wordHintSeq_) {
+    auto word = std::get<0>(elem.second);
+    auto position = std::get<1>(elem.second);
+    auto range = std::get<2>(elem.second);
+    auto next = position + range;
+    auto rangeSelf = vertexWator.equal_range(position);
+    for (auto itSelf = rangeSelf.first; itSelf != rangeSelf.second; itSelf++) {
+      auto wordSelf = std::get<0>(itSelf->second);
+      auto vrtxSelf = std::get<1>(itSelf->second);
+      auto weightR = std::get<2>(itSelf->second);
+      auto weightRO = std::get<3>(itSelf->second);
+      if (word == wordSelf) {
+        auto rangeNext = vertexWator.equal_range(next);
+        for (auto itNext = rangeNext.first; itNext != rangeNext.second;
+             itNext++) {
+          auto vrtxNext = std::get<1>(itNext->second);
+          auto ed = boost::add_edge(vrtxSelf, vrtxNext,g);
+          auto weightOld = boost::get(boost::edge_weight_t(), g, ed.first);
+          boost::put(boost::edge_weight_t(), g, ed.first, 1.0/weightR);
+          auto weightNew = boost::get(boost::edge_weight_t(), g, ed.first);
+          TRACE_VAR(weightR,weightRO,weightOld,weightNew);
+        }
+        break;
+      }
+    }
+    // add dummy end.
+    if(posLast == position + range) {
+      auto rangeSelf = vertexWator.equal_range(position);
+      for (auto itSelf = rangeSelf.first; itSelf != rangeSelf.second; itSelf++) {
+        auto wordSelf = std::get<0>(itSelf->second);
+        auto vrtxSelf = std::get<1>(itSelf->second);
+        if (word == wordSelf) {
+          auto ed = boost::add_edge(vrtxSelf,vrtxEnd,g);
+          auto weight = boost::get(boost::edge_weight_t(), g, ed.first);
+          boost::put(boost::edge_weight_t(), g, ed.first, 0.0);
+          auto weight2 = boost::get(boost::edge_weight_t(), g, ed.first);
+          TRACE_VAR(weight,weight2);
+          break;
+        }
+      }
+    }
+  }
+  
+    
+  sample_graph_writer gw(labelVertex);
+
+  std::stringstream ss;
+  boost::write_graphviz(ss, g, gw);
+  auto dotStr = ss.str();
+  boost::algorithm::replace_all(
+      dotStr, "digraph G {",
+      "digraph G { \n rankdir=LR;\n graph [charset=\"UTF-8\"];\n");
+  DUMP_VAR(dotStr);
+  // dot -v -T svg 1.dot -o 1.svg
+    
     string url("/");
     return url;
 }
