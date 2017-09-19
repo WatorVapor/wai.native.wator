0d399630f8ab62fc289270daf22e58c5a7209dc2
Wator Vapor <watorvapor@gmail.com>
2017-09-19 22:09:12 +0900

    Update phoenixword.graph.cpp

--- watorword/phoenixword.graph.cpp
+++ watorword/phoenixword.graph.cpp
@@ -188,3 +188,7 @@ void PhoenixWord::calcPrediction(const multimap<int, WordElement> &confuse) {
   }
 }
 
+string PhoenixWord::createGraph(const multimap<int, WordElement> &confuse) {
+    string url("/");
+    return url;
+}
