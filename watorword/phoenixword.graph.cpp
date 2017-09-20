0022c91b7ea787dc86f276bcf9eabac89374fc31
Wator Vapor <watorvapor@gmail.com>
2017-09-20 23:00:29 +0900

    Update phoenixword.graph.cpp

--- watorword/phoenixword.graph.cpp
+++ watorword/phoenixword.graph.cpp
@@ -28,7 +28,7 @@ struct sample_graph_writer {
     auto wordPair = labelVertex_.at(i);
     out << " [ label = <";
     out << "<FONT COLOR=\"green\" POINT-SIZE=\"20\">" << std::get<0>(wordPair) << "</FONT>";
-    out << "<FONT COLOR=\"red\" POINT-SIZE=\"10\">" << " " << std::get<1>(wordPair) << " " << std::get<2>(wordPair) << "</FONT>";
+    out << "<FONT COLOR=\"red\" POINT-SIZE=\"10\">" << " " << std::get<1>(wordPair) "" << 1.0/(std::get<1>(wordPair)) << " " << std::get<2>(wordPair) << "</FONT>";
     out << ">]";
   }
   sample_graph_writer(vector<std::tuple<string,double,double>> &labelVertex):labelVertex_(labelVertex) {
