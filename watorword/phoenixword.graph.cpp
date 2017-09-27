9d9e4026ffdff462033aa0b51f89eab63f135ff6
Wator Vapor <watorvapor@gmail.com>
2017-09-27 22:07:24 +0900

    Update phoenixword.graph.cpp

--- watorword/phoenixword.graph.cpp
+++ watorword/phoenixword.graph.cpp
@@ -246,7 +246,7 @@ string PhoenixWord::createGraph(const string &text) {
   auto title = u8"message=【" + text + u8"】";
   auto vrtxPrvrtxTitle = std::make_tuple(title, vrtxTitle,0.0,0.0);
   vertexWator.insert(std::make_pair(posLast + 10,vrtxPrvrtxTitle));
-  labelVertex.push_back(std::make_tuple(title,0.0,0.0));
+  labelVertex.push_back(std::make_tuple(title,1.0,1.0));
 
     
   // add dummy start.
