fc9b32b2b8d762b8e51e3c96f92d068ea2ecb6be
Wator Vapor <watorvapor@gmail.com>
2017-11-20 23:02:16 +0900

    Update merge.level.js

--- nodetools/merge.level.js
+++ nodetools/merge.level.js
@@ -1,1 +1,25 @@
+var levelup = require('levelup');
+var argv = require('argv');
+var args = argv.run();
+var pathDB = args.targets[0];
+console.log('pathDB =<',pathDB,'>');
 
+
+var db = levelup(pathDB);
+db.createReadStream()
+  .on('data', function (data) {
+    var value = parseInt(data.value);
+    if(value > 2) {
+    //if(true) {
+      console.log(data.key, '=', data.value)
+    }
+  })
+  .on('error', function (err) {
+    console.log('Oh my!', err)
+  })
+  .on('close', function () {
+    console.log('Stream closed')
+  })
+  .on('end', function () {
+    console.log('Stream ended')
+  })
