59211ce9b70370058315aa7026af4b367589bafb
Wator Vapor <watorvapor@gmail.com>
2017-11-20 23:12:17 +0900

    Update merge.level.js

--- nodetools/merge.level.js
+++ nodetools/merge.level.js
@@ -29,3 +29,24 @@ src.createReadStream()
     console.log('Stream ended');
   })
 
+var allNumber = 0;
+
+dist.createReadStream()
+  .on('data', function (data) {
+    var value = parseInt(data.value);
+    //console.log(data.key, '=', data.value);
+    //dist.put(data.key,data.value);
+    allNumber += 1;
+  })
+  .on('error', function (err) {
+    console.log('Oh my!', err);
+  })
+  .on('close', function () {
+    console.log('Stream closed');
+    console.log('allNumber =<',allNumber,'>');
+  })
+  .on('end', function () {
+    console.log('Stream ended');
+  })
+
+
