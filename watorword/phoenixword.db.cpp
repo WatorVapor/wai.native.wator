a2a94b88f79930fa6c28d1f921801a5248b01453
Wator Vapor <watorvapor@gmail.com>
2017-11-23 22:40:19 +0900

    Update phoenixword.db.cpp

--- watorword/phoenixword.db.cpp
+++ watorword/phoenixword.db.cpp
@@ -44,6 +44,41 @@ void PhoenixWord::pushMultiWord(const string &word) {
 void PhoenixWord::collectWord(void) {
 }
 
+void ParrotWord::commitArticle(const pt::ptree& task,const string &ws) {
+  auto wordArrays = pickupWordRanking();
+  multiWordOfOneArticle_.clear();
+  ap_ = 0;
+  for (auto word : wordArrays) {
+    DUMP_VAR(word);
+    pt::ptree upTask = task;
+    upTask.put("word", word);
+    string task_word_upPath = ws + "/task_word_up.json";
+    pt::write_json(task_word_upPath, upTask);
+
+    auto tagOpt = task.get_optional<string>("tag");
+    if (tagOpt) {
+      auto tag = tagOpt.get();
+      string wgetTaskUp("curl -6 -F \"");
+      // string wgetTaskUp("curl -F \"");
+      wgetTaskUp += "file=@";
+      wgetTaskUp += task_word_upPath;
+      wgetTaskUp += "\" ";
+      wgetTaskUp += "\"https://www.wator.xyz/wai/text/train/phoenix/";
+      wgetTaskUp += tag;
+      wgetTaskUp += "\"";
+      DUMP_VAR(wgetTaskUp);
+      ::system(wgetTaskUp.c_str());
+    }
+  }
+}
+
+void ParrotWord::commitArticle(void) {
+  multiWordOfOneArticle_.clear();
+  ap_ = 0;
+}
+
+
+
 
 void PhoenixWord::dumpPreds() {
   for (auto elem : wordSeqTopSelected_) {
