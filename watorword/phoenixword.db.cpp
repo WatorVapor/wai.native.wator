6e73b8d1d23b3b89499cf0c639a477647b5f632a
Wator Vapor <watorvapor@gmail.com>
2017-11-24 23:44:19 +0900

    Update phoenixword.db.cpp

--- watorword/phoenixword.db.cpp
+++ watorword/phoenixword.db.cpp
@@ -129,7 +129,7 @@ void PhoenixWord::commitArticle(const pt::ptree& task,const string &ws) {
       wgetTaskUp += tag;
       wgetTaskUp += "\"";
       DUMP_VAR(wgetTaskUp);
-      //::system(wgetTaskUp.c_str());
+      ::system(wgetTaskUp.c_str());
     }
   }
 }
