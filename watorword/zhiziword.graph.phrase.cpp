0c1490ed17017c381e91559544959e744f5b9d9e
Wator Vapor <watorvapor@gmail.com>
2017-12-17 15:01:22 +0900

    Update zhiziword.graph.phrase.cpp

--- watorword/zhiziword.graph.phrase.cpp
+++ watorword/zhiziword.graph.phrase.cpp
@@ -64,7 +64,7 @@ struct sample_graph_writer {
 
 
 struct sample_graph_weight_writer {
-  void operator()(std::ostream& out,Edge edge) const {
+  void operator()(std::ostream& out) const {
     out << " [ label = <";
     out << ">]";
   }
