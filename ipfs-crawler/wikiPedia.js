e0c453709bfad62e547f08f6ac37d20e26d0a0bf
Wator Vapor <watorvapor@gmail.com>
2018-01-02 12:54:04 +0900

    Update wikiPedia.js

--- ipfs-crawler/wikiPedia.js
+++ ipfs-crawler/wikiPedia.js
@@ -2,12 +2,12 @@ var bot = require('nodemw');
 // pass configuration object
 var client = new bot({
   protocol: 'https',           // Wikipedia now enforces HTTPS
-  server: 'en.wikipedia.org',  // host name of MediaWiki-powered site
+  server: 'ja.wikipedia.org',  // host name of MediaWiki-powered site
   path: '/w',                  // path to api.php script
   debug: false                 // is more verbose when set to true
 });
 
-client.getArticle('foo', function(err, data) {
+client.getArticle('日本', function(err, data) {
   // error handling
   if (err) {
     console.error('err=<',err,'>');
