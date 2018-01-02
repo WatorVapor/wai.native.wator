e42dcc25c809aca29850751047ca1194eeaea0d9
Wator Vapor <watorvapor@gmail.com>
2018-01-02 22:12:45 +0900

    Update wikiPedia.js

--- ipfs-crawler/wikiPedia.js
+++ ipfs-crawler/wikiPedia.js
@@ -18,65 +18,69 @@ module.exports = class WikiCrawler {
     if(option.seed) {
       this.seed = option.seed;
     }
-    //console.log('WikiCrawler::constructor this=<',this,'>');
-    runOnce(){
-      https.get(this.root + this.seed, (resp) => {
-        let data = '';
-        resp.on('data', (chunk) => {
-          data += chunk;
-        });
-        resp.on('end', () => {
-          //console.log('end: data=<',data,'>');
-          this.parseHTML_(data);
-        });
-      }).on('error', (err) => {
-        console.log('Error: err=<',err,'>');
-      });
-    }
-    parseHTML_(data) {
-      var plainText = '';
-      var hrefsLinks = [];
-      try {
-        const $ = cheerio.load(data);
-        $('p').each( (i, elem) => {
-          //console.log('i=<',i,'>');
-          //console.log('elem=<',elem,'>');
-          elem.children.forEach( (value, index, ar) => {
-            //console.log('value=<',value,'>');
-            if(value.type === 'text') {
-              //console.log('value.data=<',value.data,'>');
-              plainText += value.data;
-            }
-            if(value.type === 'tag' && value.name === 'a') {
-              //console.log('value=<',value,'>');
-              value.children.forEach( (valueA, indexA, arA) => {
-                if(valueA.type === 'text') {
-                  //console.log('valueA.data=<',valueA.data,'>');
-                  plainText += valueA.data;
-                }
-              });
-              if(value.attribs.href.startsWith(this.prefix)) {
-                if(this.replace) {
-                  let newHref = value.attribs.href.replace(this.prefix,this.replace);
-                  //console.log('newHref=<',newHref,'>');
-                  hrefsLinks.push(this.root + newHref);
-                } else {
-                  hrefsLinks.push(this.root + value.attribs.href);
-                }
-              } else {
-                //console.log('value.attribs.href=<',value.attribs.href,'>');
-              }
-            }
-            //console.log('index=<',index,'>');
-          });
-        });
-      } catch(e) {
-        console.log('e=<',e,'>');
-      }
-      console.log('plainText=<',plainText,'>');
-      console.log('hrefsLinks=<',hrefsLinks,'>');
-    }    
+    console.log('WikiCrawler::constructor this=<',this,'>');
   }
+  
+  
+  runOnce(){
+    https.get(this.root + this.seed, (resp) => {
+      let data = '';
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+      resp.on('end', () => {
+        //console.log('end: data=<',data,'>');
+        this.parseHTML_(data);
+      });
+    }).on('error', (err) => {
+      console.log('Error: err=<',err,'>');
+    });
+  }
+  
+  
+  parseHTML_(data) {
+    var plainText = '';
+    var hrefsLinks = [];
+    try {
+      const $ = cheerio.load(data);
+      $('p').each( (i, elem) => {
+        //console.log('i=<',i,'>');
+        //console.log('elem=<',elem,'>');
+        elem.children.forEach( (value, index, ar) => {
+          //console.log('value=<',value,'>');
+          if(value.type === 'text') {
+            //console.log('value.data=<',value.data,'>');
+            plainText += value.data;
+          }
+          if(value.type === 'tag' && value.name === 'a') {
+            //console.log('value=<',value,'>');
+            value.children.forEach( (valueA, indexA, arA) => {
+              if(valueA.type === 'text') {
+                //console.log('valueA.data=<',valueA.data,'>');
+                plainText += valueA.data;
+              }
+            });
+            if(value.attribs.href.startsWith(this.prefix)) {
+              if(this.replace) {
+                let newHref = value.attribs.href.replace(this.prefix,this.replace);
+                //console.log('newHref=<',newHref,'>');
+                hrefsLinks.push(this.root + newHref);
+              } else {
+                hrefsLinks.push(this.root + value.attribs.href);
+              }
+            } else {
+              //console.log('value.attribs.href=<',value.attribs.href,'>');
+            }
+          }
+          //console.log('index=<',index,'>');
+        });
+      });
+    } catch(e) {
+      console.log('e=<',e,'>');
+    }
+    console.log('plainText=<',plainText,'>');
+    console.log('hrefsLinks=<',hrefsLinks,'>');
+  }    
 }
 
 
