d1f4cfd6043a1809d0afe29016afc4f78866bfb7
Wator Vapor <watorvapor@gmail.com>
2018-01-02 18:56:48 +0900

    Update app.js

--- ipfs-crawler/app.js
+++ ipfs-crawler/app.js
@@ -1,10 +1,15 @@
 var ipfsAPI = require('ipfs-api');
-//var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
 var ipfs = ipfsAPI({host: 'localhost', port: '5001', protocol: 'http'});
 
+/*
 ipfs.util.addFromFs('/tmp/test.txt',{ recursive: false},function(err, result) {
   if (err) {
     throw err;
   }
   console.log('result=<',result,'>');
 });
+*/
+
+var client = redis.createClient();
+var redis = require('redis');
+console.log('redis=<',redis,'>');
