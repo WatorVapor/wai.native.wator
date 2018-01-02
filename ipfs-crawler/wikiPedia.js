0f802d02e4f7159e1ebdf994cb90b8a0767256a4
Wator Vapor <watorvapor@gmail.com>
2018-01-02 14:15:24 +0900

    Update wikiPedia.js

--- ipfs-crawler/wikiPedia.js
+++ ipfs-crawler/wikiPedia.js
@@ -1,58 +1,14 @@
-var bot = require('nodemw');
-// pass configuration object
-var clientJa = new bot({
-  protocol: 'https',           // Wikipedia now enforces HTTPS
-  server: 'ja.wikipedia.org',  // host name of MediaWiki-powered site
-  path: '/w',                  // path to api.php script
-  debug: false                 // is more verbose when set to true
+const cheerio = require('cheerio');
+const https = require('https');
+
+https.get('', (resp) => {
+  let data = '';
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+  resp.on('end', () => {
+    console.log('end: data=<',data,'>');
+  });
+}).on('error', (err) => {
+  console.log('Error: err=<',err,'>');
 });
-
-console.log('clientJa.getConfig()=<',clientJa.getConfig(),'>');
-
-
-/*
-clientJa.getBacklinks('日本', function(err, data) {
-  // error handling
-  if (err) {
-    console.error('err=<',err,'>');
-    return;
-  }
-  //console.log('data=<',data,'>');
-  for(var i = 0;i < data.length;i++) {
-    var page = data[i];
-    console.log('page=<',page,'>');
-  }
-});
-*/
-
-var clientZh = new bot({
-  protocol: 'https',           // Wikipedia now enforces HTTPS
-  server: 'zh.wikipedia.org',  // host name of MediaWiki-powered site
-  path: '/w',                  // path to api.php script
-  debug: false                 // is more verbose when set to true
-});
-console.log('clientZh.getConfig()=<',clientZh.getConfig(),'>');
-
-/*
-clientZh.getArticle('中国', function(err, data) {
-  // error handling
-  if (err) {
-    console.error('err=<',err,'>');
-    return;
-  }
-  console.log('data=<',data,'>');
-});
-*/
-
-/*
-clientZh.fetchUrl('https://zh.wikipedia.org/zh-cn/%E6%AC%A1%E5%8E%9F%E5%AD%90%E7%B2%92%E5%AD%90', function(err, data) {
-  // error handling
-  if (err) {
-    console.error('err=<',err,'>');
-    return;
-  }
-  console.log('data=<',data,'>');
-});
-*/
-
-
