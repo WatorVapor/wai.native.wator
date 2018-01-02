c9ac638f5ad7466b81361457b89d5649616b4d5f
Wator Vapor <watorvapor@gmail.com>
2018-01-02 14:31:35 +0900

    Update wikiPedia.js

--- ipfs-crawler/wikiPedia.js
+++ ipfs-crawler/wikiPedia.js
@@ -1,4 +1,3 @@
-const cheerio = require('cheerio');
 const https = require('https');
 
 https.get('https://zh.wikipedia.org/zh-cn/%E5%8E%9F%E5%AD%90', (resp) => {
@@ -8,12 +7,26 @@ https.get('https://zh.wikipedia.org/zh-cn/%E5%8E%9F%E5%AD%90', (resp) => {
   });
   resp.on('end', () => {
     //console.log('end: data=<',data,'>');
-    const $ = cheerio.load(data);
-    $('p').each( (i, elem)=> {
-      console.log('i=<',i,'>');
-      console.log('elem=<',elem,'>');
-    });
+    parseHTML(data);
   });
 }).on('error', (err) => {
   console.log('Error: err=<',err,'>');
 });
+
+const cheerio = require('cheerio');
+function parseHTML($data) {
+  try {
+    const $ = cheerio.load(data);
+    $('p').each( (i, elem)=> {
+      console.log('i=<',i,'>');
+      console.log('elem=<',elem,'>');
+      elem.children.forEach(function(value, index, ar){
+        console.log('value=<',value,'>');
+        console.log('index=<',index,'>');
+        console.log('ar=<',ar,'>');
+      });
+    });
+  } catch(e) {
+    console.log('e=<',e,'>');
+  }
+}
