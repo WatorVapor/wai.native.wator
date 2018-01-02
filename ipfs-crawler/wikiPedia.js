ac8b8632dffbb11dd5aca5c7a462ff3044db6159
Wator Vapor <watorvapor@gmail.com>
2018-01-02 12:58:47 +0900

    Update wikiPedia.js

--- ipfs-crawler/wikiPedia.js
+++ ipfs-crawler/wikiPedia.js
@@ -7,7 +7,7 @@ var client = new bot({
   debug: false                 // is more verbose when set to true
 });
 
-client.getArticleRevisions('日本', function(err, data) {
+client.fetchUrl('日本', function(err, data) {
   // error handling
   if (err) {
     console.error('err=<',err,'>');
