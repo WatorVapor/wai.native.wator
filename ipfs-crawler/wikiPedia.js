2ac222c6aa689f0dca19cc689d2f4356a7a200af
Wator Vapor <watorvapor@gmail.com>
2018-01-02 13:14:31 +0900

    Update wikiPedia.js

--- ipfs-crawler/wikiPedia.js
+++ ipfs-crawler/wikiPedia.js
@@ -7,7 +7,7 @@ var client = new bot({
   debug: false                 // is more verbose when set to true
 });
 
-client.getBacklinks('冷戦', function(err, data) {
+client.getBacklinks('中性子爆弾', function(err, data) {
   // error handling
   if (err) {
     console.error('err=<',err,'>');
