88008b32dc9c73c42b51b419b8f6d9bb338075d8
Wator Vapor <watorvapor@gmail.com>
2018-01-02 13:21:21 +0900

    Update wikiPedia.js

--- ipfs-crawler/wikiPedia.js
+++ ipfs-crawler/wikiPedia.js
@@ -1,13 +1,33 @@
 var bot = require('nodemw');
 // pass configuration object
-var client = new bot({
+var clientJa = new bot({
   protocol: 'https',           // Wikipedia now enforces HTTPS
   server: 'ja.wikipedia.org',  // host name of MediaWiki-powered site
   path: '/w',                  // path to api.php script
   debug: false                 // is more verbose when set to true
 });
 
-client.getBacklinks('日本', function(err, data) {
+clientJa.getBacklinks('日本', function(err, data) {
+  // error handling
+  if (err) {
+    console.error('err=<',err,'>');
+    return;
+  }
+  //console.log('data=<',data,'>');
+  for(var i = 0;i < data.length;i++) {
+    var page = data[i];
+    console.log('page=<',page,'>');
+  }
+});
+
+var clientZh = new bot({
+  protocol: 'https',           // Wikipedia now enforces HTTPS
+  server: 'zh.wikipedia.org',  // host name of MediaWiki-powered site
+  path: '/zh-cn/w',                  // path to api.php script
+  debug: false                 // is more verbose when set to true
+});
+
+clientZh.getBacklinks('中国', function(err, data) {
   // error handling
   if (err) {
     console.error('err=<',err,'>');
