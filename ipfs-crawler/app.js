8404f9a11637d34c9f86d276a76cd5000e091a11
Wator Vapor <watorvapor@gmail.com>
2018-01-02 17:52:52 +0900

    Update app.js

--- ipfs-crawler/app.js
+++ ipfs-crawler/app.js
@@ -1,5 +1,6 @@
 var ipfsAPI = require('ipfs-api');
-var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
+//var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
+var ipfs = ipfsAPI({host: 'localhost', port: '5001', protocol: 'http'});
 
 ipfs.util.addFromFs('/tmp/test.txt',{ recursive: false},function(err, result) {
   if (err) {
