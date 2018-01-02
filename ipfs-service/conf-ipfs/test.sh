d95bffdaf692ba2b03def30123af80780e3cd631
Wator Vapor <watorvapor@gmail.com>
2018-01-02 12:21:33 +0900

    Update test.sh

--- ipfs-service/conf-ipfs/test.sh
+++ ipfs-service/conf-ipfs/test.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-ipfs init
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://localhost:3000"]'
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
+ipfs diag sys
