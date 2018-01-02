7a52ee54ea3dbe11440dac3089c7902b758f194b
Wator Vapor <watorvapor@gmail.com>
2018-01-02 21:58:56 +0900

    Update app.js

--- ipfs-crawler/app.js
+++ ipfs-crawler/app.js
@@ -1,6 +1,17 @@
 var ipfsAPI = require('ipfs-api');
 var ipfs = ipfsAPI({host: 'localhost', port: '5001', protocol: 'http'});
 
+var wiki = require('./wikiPedia.js');
+var zhOption =  {
+  prefix:'/wiki/',
+  replace:'/zh-cn/',
+  root:'https://zh.wikipedia.org',
+  seed:'/zh-cn/%E5%8C%96%E5%AD%B8%E5%85%83%E7%B4%A0'
+}
+var zhWiki = new wiki(zhOption);
+console.log('zhWiki=<',zhWiki,'>');
+
+
 /*
 ipfs.util.addFromFs('/tmp/test.txt',{ recursive: false},function(err, result) {
   if (err) {
@@ -10,6 +21,4 @@ ipfs.util.addFromFs('/tmp/test.txt',{ recursive: false},function(err, result) {
 });
 */
 
-var redis = require('redis');
-var client = redis.createClient();
 //console.log('client=<',client,'>');
