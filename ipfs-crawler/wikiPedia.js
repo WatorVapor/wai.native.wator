e22b277c52b5cc7ba0eeb19621656a64a8617192
Wator Vapor <watorvapor@gmail.com>
2018-01-03 21:56:56 +0900

    Update wikiPedia.js

--- ipfs-crawler/wikiPedia.js
+++ ipfs-crawler/wikiPedia.js
@@ -124,8 +124,8 @@ module.exports = class WikiCrawler {
     }
     this.saveDoneWiki_(url,plainText);
     this.saveLinkedWiki_(hrefsLinks);
-    //console.log('plainText=<',plainText,'>');
-    //console.log('hrefsLinks=<',hrefsLinks,'>');
+    console.log('plainText=<',plainText,'>');
+    console.log('hrefsLinks=<',hrefsLinks,'>');
   }
   
   
