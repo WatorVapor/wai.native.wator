01ddde86f430e898ef15e67ddfc63b68979cc4d0
Wator Vapor <watorvapor@gmail.com>
2018-01-03 22:01:05 +0900

    Update wikiPedia.js

--- ipfs-crawler/wikiPedia.js
+++ ipfs-crawler/wikiPedia.js
@@ -86,6 +86,7 @@ module.exports = class WikiCrawler {
     var plainText = '';
     var hrefsLinks = [];
     try {
+      console.log('data=<',data,'>');
       const $ = cheerio.load(data);
       $('p').each( (i, elem) => {
         //console.log('i=<',i,'>');
