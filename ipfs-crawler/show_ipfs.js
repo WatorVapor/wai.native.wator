934a42482bd7f27b33f06ee3a4ff0d43fbdda331
Wator Vapor <watorvapor@gmail.com>
2018-01-04 10:17:30 +0900

    Update show_ipfs.js

--- ipfs-crawler/show_ipfs.js
+++ ipfs-crawler/show_ipfs.js
@@ -2,7 +2,10 @@ var redis = require('redis');
 var ipfsAPI = require('ipfs-api');
 
 var ipfs = ipfsAPI({host: 'localhost', port: '5001', protocol: 'http'});
-var refs = ipfs.refs.local();
+var refs = ipfs.refs.local((err,result)=>{
+  console.log('err=<',err,'>');
+  console.log('result=<',result,'>');
+});
 console.log('refs=<',refs,'>');
 
 
