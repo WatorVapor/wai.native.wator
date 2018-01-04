c30bfc1c0e37733a6b617458e9b4a5838fd6c04c
Wator Vapor <watorvapor@gmail.com>
2018-01-04 10:22:12 +0900

    Update show_ipfs.js

--- ipfs-crawler/show_ipfs.js
+++ ipfs-crawler/show_ipfs.js
@@ -2,12 +2,31 @@ var redis = require('redis');
 var ipfsAPI = require('ipfs-api');
 
 var ipfs = ipfsAPI({host: 'localhost', port: '5001', protocol: 'http'});
+
+/*
 var refs = ipfs.refs.local((err,result)=>{
   console.log('err=<',err,'>');
   console.log('result=<',result,'>');
 });
 console.log('refs=<',refs,'>');
+*/
 
 
-//var client = redis.createClient();
-  
+const redisKeyPrefix = '/wator/wai/crawler/wiki';
+const redisKeyPrefixDone = '/wator/wai/crawler/wiki/done';
+const redisKeyPrefixTodo = '/wator/wai/crawler/wiki/todo';
+const redisKeyPrefixIpfs = '/wator/wai/wiki/text';
+
+
+var client = redis.createClient();
+
+client.keys(redisKeyPrefixIpfs + '/*', function (err, keys) {
+  if (err) { 
+    return console.log('err=<',err,'>');
+  }
+  try {
+    console.log('keys.length=<',keys.length,'>');
+  } catch(e) {
+    console.log('err=<',err,'>');
+  }
+}); 
