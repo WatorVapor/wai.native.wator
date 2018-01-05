bd9be98058abcad23c9fbcdd7772f9e3e4f9fda4
Wator Vapor <watorvapor@gmail.com>
2018-01-06 03:41:51 +0900

    Update pub.api.js

--- ipfs-crawler/pub.api.js
+++ ipfs-crawler/pub.api.js
@@ -2,20 +2,11 @@ var ipfsAPI = require('ipfs-api');
 var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
 //console.log('ipfs=<',ipfs,'>');
 
-var key = 'hello.11';
-var data = {test:true};
-ipfs.dht.put(key, JSON.stringify(data), function (err) {
-  if(err) {
-    console.log('put err=<',err,'>');
-  } else {
-    console.log('put err=<',err,'>');
+const topic = 'fruit-of-the-day';
+const msg = new Buffer('banana');
+ipfs.pubsub.publish(topic, msg, (err) => {
+  if (err) {
+    throw err
   }
-});
-
-ipfs.dht.get(key, { timeout: '100ms' },function (err, value) {
-  if(err) {
-    console.log('get err=<',err,'>');
-  } else {
-    console.log('get value=<',value,'>');
-  }  
+  console.log('sented msg=<',msg,'>');
 });
