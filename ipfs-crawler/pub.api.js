e9621c59edea1831d0708acbc4d94e437664df69
Wator Vapor <watorvapor@gmail.com>
2018-01-05 18:39:13 +0000

    ipfs api test

---
+++ ipfs-crawler/pub.api.js
@@ -0,0 +1,21 @@
+var ipfsAPI = require('ipfs-api');
+var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
+//console.log('ipfs=<',ipfs,'>');
+
+var key = 'hello.11';
+var data = {test:true};
+ipfs.dht.put(key, JSON.stringify(data), function (err) {
+  if(err) {
+    console.log('put err=<',err,'>');
+  } else {
+    console.log('put err=<',err,'>');
+  }
+});
+
+ipfs.dht.get(key, { timeout: '100ms' },function (err, value) {
+  if(err) {
+    console.log('get err=<',err,'>');
+  } else {
+    console.log('get value=<',value,'>');
+  }  
+});
