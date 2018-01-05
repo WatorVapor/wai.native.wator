5a72644e373e0e713eb1bd889e5919ba0b228a7b
Wator Vapor <watorvapor@gmail.com>
2018-01-05 18:06:05 +0900

    Update test.dht.api.js

--- ipfs-crawler/test.dht.api.js
+++ ipfs-crawler/test.dht.api.js
@@ -2,8 +2,9 @@ var ipfsAPI = require('ipfs-api');
 var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
 //console.log('ipfs=<',ipfs,'>');
 
+var key = 'hello.11';
 var data = {test:true};
-ipfs.dht.put('hello.11', JSON.stringify(data), function (err) {
+ipfs.dht.put(key, JSON.stringify(data), function (err) {
   if(err) {
     console.log('err=<',err,'>');
   } else {
