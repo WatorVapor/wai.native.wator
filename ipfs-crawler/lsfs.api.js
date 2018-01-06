6e0c9fa73e6fec9b058977eb58fe07fcdb0cd416
Wator Vapor <watorvapor@gmail.com>
2018-01-06 10:36:14 +0000

    ipfs api test

---
+++ ipfs-crawler/lsfs.api.js
@@ -0,0 +1,12 @@
+var ipfsAPI = require('ipfs-api');
+var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
+//console.log('ipfs=<',ipfs,'>');
+
+const topic = 'fruit-of-the-day-wator';
+const msg = new Buffer('banana');
+ipfs.pubsub.publish(topic, msg, (err) => {
+  if (err) {
+    throw err
+  }
+  console.log('sented msg=<',msg,'>');
+});
