3358f4d80bf656b61961b0a9fc7c4f0ea6369973
Wator Vapor <watorvapor@gmail.com>
2018-01-08 22:09:18 +0900

    Update pub.api.js

--- ipfs-crawler/pub.api.js
+++ ipfs-crawler/pub.api.js
@@ -1,5 +1,6 @@
 var ipfsAPI = require('ipfs-api');
-var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
+//var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
+var ipfs = ipfsAPI('master.ipfs.wator.xyz', '5001', {protocol: 'http'});
 //console.log('ipfs=<',ipfs,'>');
 
 const topic = 'fruit-of-the-day-wator';
