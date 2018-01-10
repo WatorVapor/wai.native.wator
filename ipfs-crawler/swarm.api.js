f18916cb55517604b73f3d5e0447c025b5ad2466
Wator Vapor <watorvapor@gmail.com>
2018-01-10 22:20:50 +0900

    Update swarm.api.js

--- ipfs-crawler/swarm.api.js
+++ ipfs-crawler/swarm.api.js
@@ -3,6 +3,14 @@ var ipfsAPI = require('ipfs-api');
 var ipfs = ipfsAPI('master.ipfs.wator.xyz', '5001', {protocol: 'http'});
 //console.log('ipfs=<',ipfs,'>');
 
+ipfs.swarm.addrs(function (err, addrs) {
+  if (err) {
+    throw err;
+  }
+  console.log('addrs=<',addrs,'>');
+})
+
+
 ipfs.swarm.peers(function (err, peerInfos) {
   if (err) {
     throw err;
