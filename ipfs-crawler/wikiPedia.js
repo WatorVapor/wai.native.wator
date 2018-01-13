e6f9c458e0bce7694a75c961aab7f067980fe6d2
Wator Vapor <watorvapor@gmail.com>
2018-01-13 17:08:21 +0900

    Update wikiPedia.js

--- ipfs-crawler/wikiPedia.js
+++ ipfs-crawler/wikiPedia.js
@@ -19,7 +19,7 @@ const redisKeyPrefixIpfs = '/wator/wai/wiki/text';
 const filterWiki = [
   '/Wikipedia:','/Help:','/Portal:',
   '/Special:', '/Project:','/Category:',
-  '/Talk:','Template:','/File:'
+  '/Talk:','Template:','/File:','/Template_talk:'
 ];
 
 
