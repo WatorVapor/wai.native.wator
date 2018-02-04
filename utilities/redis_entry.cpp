21e09a2d9f3f860148997b32c3e6ff8ba5c5835e
Wator Vapor <watorvapor@gmail.com>
2018-02-04 22:58:34 +0900

    Update redis_entry.cpp

--- utilities/redis_entry.cpp
+++ utilities/redis_entry.cpp
@@ -1,2 +1,28 @@
 #include "redis_entry.hpp"
 #include "log.hpp"
+
+const string strConstTrainChannelName("wai.train");
+
+void redis_main(void) {
+  boost::asio::ip::address address = boost::asio::ip::address::from_string("master.redis.wator.xyz");
+  const unsigned short port = 6379;
+  boost::asio::ip::tcp::endpoint endpoint(address, port);
+  boost::asio::io_service ioService;
+  RedisClient client(ioService);
+  redisclient::RedisAsyncClient publisher(ioService);
+  publisher.connect(endpoint, [&](boost::system::error_code ec) {
+    if(ec) {
+      DUMP_VAR(ec);
+    } else {
+    }
+  });
+  redisclient::RedisAsyncClient subscriber(ioService);
+  subscriber.connect(endpoint, [&](boost::system::error_code ec){
+    if(ec) {
+      DUMP_VAR(ec);
+    } else {
+      subscriber.subscribe(strConstTrainChannelName,std::bind(&RedisClient::onMessageAPI, &client, std::placeholders::_1));
+    }
+  });  
+  ioService.run();
+}
