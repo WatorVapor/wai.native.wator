2b9ac3d1679f7e3998d70ad11a4527214c640c45
Wator Vapor <watorvapor@gmail.com>
2018-02-06 21:38:18 +0900

    Update redis_entry.cpp

--- watorword/redis_entry.cpp
+++ watorword/redis_entry.cpp
@@ -4,27 +4,31 @@
 const string strConstTrainChannelName("wai.train");
 
 void redis_main(void) {
-  boost::asio::ip::address address = boost::asio::ip::address::from_string("master.redis.wator.xyz");
-  const unsigned short port = 6379;
-  boost::asio::ip::tcp::endpoint endpoint(address, port);
-  boost::asio::io_service ioService;
-  RedisEntryClient client(ioService);
-  redisclient::RedisAsyncClient publisher(ioService);
-  publisher.connect(endpoint, [&](boost::system::error_code ec) {
-    if(ec) {
-      DUMP_VAR(ec);
-    } else {
-    }
-  });
-  redisclient::RedisAsyncClient subscriber(ioService);
-  subscriber.connect(endpoint, [&](boost::system::error_code ec){
-    if(ec) {
-      DUMP_VAR(ec);
-    } else {
-      subscriber.subscribe(strConstTrainChannelName,std::bind(&RedisEntryClient::onMessageAPI, &client, std::placeholders::_1));
-    }
-  });  
-  ioService.run();
+  try{
+    boost::asio::ip::address address = boost::asio::ip::address::from_string("master.redis.wator.xyz");
+    const unsigned short port = 6379;
+    boost::asio::ip::tcp::endpoint endpoint(address, port);
+    boost::asio::io_service ioService;
+    RedisEntryClient client(ioService);
+    redisclient::RedisAsyncClient publisher(ioService);
+    publisher.connect(endpoint, [&](boost::system::error_code ec) {
+      if(ec) {
+        DUMP_VAR(ec);
+      } else {
+      }
+    });
+    redisclient::RedisAsyncClient subscriber(ioService);
+    subscriber.connect(endpoint, [&](boost::system::error_code ec){
+      if(ec) {
+        DUMP_VAR(ec);
+      } else {
+        subscriber.subscribe(strConstTrainChannelName,std::bind(&RedisEntryClient::onMessageAPI, &client, std::placeholders::_1));
+      }
+    });  
+    ioService.run();
+  } catch(std::exception e) {
+    DUMP_VAR(e.what());
+  }
 }
 void RedisEntryClient::onMessageAPI(const std::vector<char> &buf) {
   string msg(buf.begin(),buf.end());
