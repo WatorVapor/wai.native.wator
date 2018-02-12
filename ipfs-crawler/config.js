2babee282f4d9eb393cc5269863b98b96f768c67
Wator Vapor <watorvapor@gmail.com>
2018-02-12 00:46:55 +0000

    setting

---
+++ ipfs-crawler/config.js
@@ -0,0 +1,60 @@
+
+/*
+var ipfsAPI = require('ipfs-api');
+var ipfs = ipfsAPI({host: 'localhost', port: '5001', protocol: 'http'});
+var refs = ipfs.refs.local((err,result)=>{
+  console.log('err=<',err,'>');
+  console.log('result=<',result,'>');
+});
+console.log('refs=<',refs,'>');
+*/
+
+
+var redis = require('redis');
+
+const redisOption = {
+  port:6379,
+  host:'master.redis.wator.xyz',
+  family:'IPv6'
+};
+
+var client = redis.createClient(redisOption);
+
+const redisKeyPrefix = '/wator/wai/crawler/wiki';
+const redisKeyPrefixDone = '/wator/wai/crawler/wiki/done';
+const redisKeyPrefixTodo = '/wator/wai/crawler/wiki/todo';
+const redisKeyPrefixIpfs = '/wator/wai/wiki/text';
+
+client.keys(redisKeyPrefixIpfs + '/*', function (err, keys) {
+  if (err) { 
+    return console.log('err=<',err,'>');
+  }
+  try {
+    console.log(redisKeyPrefixIpfs,'keys.length=<',keys.length,'>');
+    console.log(redisKeyPrefixIpfs,'keys[keys.length -1]=<',keys[keys.length -1],'>');
+  } catch(e) {
+    console.log('err=<',err,'>');
+  }
+}); 
+
+client.keys(redisKeyPrefixDone + '/*', function (err, keys) {
+  if (err) { 
+    return console.log('err=<',err,'>');
+  }
+  try {
+    console.log(redisKeyPrefixDone,'keys.length=<',keys.length,'>');
+  } catch(e) {
+    console.log('err=<',err,'>');
+  }
+}); 
+
+client.keys(redisKeyPrefixTodo + '/*', function (err, keys) {
+  if (err) { 
+    return console.log('err=<',err,'>');
+  }
+  try {
+    console.log(redisKeyPrefixTodo,'keys.length=<',keys.length,'>');
+  } catch(e) {
+    console.log('err=<',err,'>');
+  }
+}); 
