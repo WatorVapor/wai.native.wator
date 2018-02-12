11c240a1c793334d80f0a3d6f00af7e7947e8a77
Wator Vapor <watorvapor@gmail.com>
2018-02-12 10:08:35 +0900

    Update config.js

--- ipfs-crawler/config.js
+++ ipfs-crawler/config.js
@@ -2,7 +2,7 @@ var ipfsAPI = require('ipfs-api');
 //var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
 var ipfs = ipfsAPI('master.ipfs.wator.xyz', '5001', {protocol: 'http'});
 
-ipfs.config.get('*',(err, config) => {
+ipfs.config.get('API.HTTPHeaders.Access-Control-Allow-Origin',(err, config) => {
   if (err) {
     throw err
   }
