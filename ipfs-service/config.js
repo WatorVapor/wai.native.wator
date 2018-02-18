3a67de8521f3a7cd860bed45322ad3ce70c74428
Wator Vapor <watorvapor@gmail.com>
2018-02-18 20:33:29 +0900

    Create config.js

---
+++ ipfs-service/config.js
@@ -0,0 +1,18 @@
+var ipfsAPI = require('ipfs-api');
+var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
+//var ipfs = ipfsAPI('master.ipfs.wator.xyz', '5001', {protocol: 'http'});
+
+ipfs.config.set('Datastore.StorageMax','16GB',(err, reply) => {
+  if (err) {
+    throw err
+  }
+  console.log('get::reply=<',reply,'')
+})
+
+
+ipfs.config.get('Datastore.StorageMax',(err, config) => {
+  if (err) {
+    throw err
+  }
+  console.log('get::config=<',config,'')
+})
