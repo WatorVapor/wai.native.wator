6c9ae06817e77a9095dc5d6d1cf979c466f978ec
Wator Vapor <watorvapor@gmail.com>
2018-02-18 16:57:39 +0900

    Update wai.cpp

--- tools/wai.cpp
+++ tools/wai.cpp
@@ -3,15 +3,20 @@
 #include <thread>
 using namespace std;
 
+bool loadMasterDB(void);
+void unloadMasterDB(void);
+
 //void api_upd_main(void);
 void redis_sub_main(void);
 void redis_pub_main(void);
 int main(int ac, char *av[]) {
   //std::thread t1(api_upd_main);
+  loadMasterDB();
   std::thread t2(redis_sub_main);
   std::thread t3(redis_pub_main);
   //t1.join();
   t2.join();
   t3.join();
+  unloadMasterDB();
   return 0;
 }
