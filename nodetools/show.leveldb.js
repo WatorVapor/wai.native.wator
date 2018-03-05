74c7392a89521e517fe487c920829d01df589dd7
Wator Vapor <watorvapor@gmail.com>
2018-03-05 23:46:21 +0900

    Create show.leveldb.js

---
+++ nodetools/show.leveldb.js
@@ -0,0 +1,28 @@
+var levelup = require('levelup');
+var argv = require('argv');
+var args = argv.run();
+var pathDB = args.targets[0];
+console.log('pathDB =<',pathDB,'>');
+var thred = args.targets[1];
+console.log('thred =<',thred,'>');
+
+
+var db = levelup(pathDB);
+db.createReadStream()
+  .on('data', function (data) {
+    var value = parseInt(data.value);
+    //console.log(data.key, '=', data.value)
+    if(value > parseInt(thred)) {
+    //if(true) {
+      console.log(data.key, '=', data.value)
+    }
+  })
+  .on('error', function (err) {
+    console.log('Oh my!', err)
+  })
+  .on('close', function () {
+    console.log('Stream closed')
+  })
+  .on('end', function () {
+    console.log('Stream ended')
+  })
