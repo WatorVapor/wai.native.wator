bc44d1309710657659bc3f780a5046d33e2364b9
Wator Vapor <watorvapor@gmail.com>
2018-03-05 23:30:22 +0900

    Update phoneme.leveldb.js

--- nodetools/phoneme.leveldb.js
+++ nodetools/phoneme.leveldb.js
@@ -1,1 +1,11 @@
+var levelup = require('levelup');
+var argv = require('argv');
+var args = argv.run();
+var srcJson = args.targets[0];
+console.log('srcJson =<',srcJson,'>');
+
+var distDB = args.targets[1];
+console.log('distDB =<',distDB,'>');
+var dist = levelup(distDB);
+
 
