a701b89e030249b5ef0a8311a9a61cd88c779e83
Wator Vapor <watorvapor@gmail.com>
2018-03-05 23:36:54 +0900

    Update phoneme.leveldb.js

--- nodetools/phoneme.leveldb.js
+++ nodetools/phoneme.leveldb.js
@@ -1,6 +1,6 @@
 var levelup = require('levelup');
-var phonemePath = '/watorvapor/rsaauth/wator/pubKeys/b3f3e38d032b4b956d3c4591f40aaff21fb37ec1340626eaf3b35e84e23ced763c18502925f0a16d5b4a10284b83d5e15cbc1cbcec593e095c0776d287e8f36/wai/phoneme_ja.json';
-var phoneme = require(phonemePath)
+var phonemePath = '/watorvapor/rsaauth/wator/pubKeys/b3f3e38d032b4b956d3c4591f40aaff21fb337ec1340626eaf3b35e84e23ced763c18502925f0a16d5b4a10284b83d5e15cbc1cbcec593e095c0776d287e8f36/wai/phoneme_ja.json';
+var phoneme = require(phonemePath);
 console.log('phoneme =<',phoneme,'>');
 
 //var argv = require('argv');
