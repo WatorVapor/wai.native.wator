059c7bde3270b12cb3a991edabf0d62945481d1e
Wator Vapor <watorvapor@gmail.com>
2018-03-09 22:43:41 +0900

    Update ttsmaster.cpp

--- watorword/ttsmaster.cpp
+++ watorword/ttsmaster.cpp
@@ -16,34 +16,53 @@ using namespace std;
 
 
 TTSMaster::TTSMaster(const string &path) {
-  master_db_path_ = path ;
+  phrase_master_db_path_ = path + "/phrase";
+  hanzi_master_db_path_ = path + "/hanzi";
+  phoneme_master_db_path_ = path + "/phoneme";
 }
 
 TTSMaster::~TTSMaster() {}
 
-void TTSMaster::openDB() {
-  if (master_ == nullptr) {
-    leveldb::Options options;
-    options.create_if_missing = true;
-    options.max_open_files = 512;
-    options.paranoid_checks = true;
-    options.compression = leveldb::kNoCompression;
-    auto status = leveldb::DB::Open(options, master_db_path_, &master_);
-    if (status.ok() == false) {
-      DUMP_VAR(status.ToString());
-      master_ = nullptr;
-    }
-  }
-}
 
-void TTSMaster::closeDB() {
-  if (master_ != nullptr) {
-    delete master_;
-    master_ = nullptr;
-  }
-}
 string TTSMaster::getPhoneme(const string &word) {
   string result;
   return result;
 }
 
+
+
+
+
+#define OPEN_DB(x) { \
+  if (x#master_ == nullptr) {
+    leveldb::Options options;
+    options.create_if_missing = true;
+    options.max_open_files = 512;
+    options.paranoid_checks = true;
+    options.compression = leveldb::kNoCompression;
+    auto status = leveldb::DB::Open(options, x#master_db_path_, &x#master_);
+    if (status.ok() == false) {
+      DUMP_VAR(status.ToString());
+      x#master_ = nullptr;
+    }
+  }
+}
+
+#define CLOSE_DB(x) { \
+  if (x#master_ != nullptr) {
+    delete x#master_;
+    x#master_ = nullptr;
+  }
+}
+
+void TTSMaster::openDB() {
+  OPEN_DB(phrase);
+  OPEN_DB(hanzi);
+  OPEN_DB(phoneme);
+}
+
+void TTSMaster::closeDB() {
+  CLOSE_DB(phrase);
+  CLOSE_DB(hanzi);
+  CLOSE_DB(phoneme);
+}
