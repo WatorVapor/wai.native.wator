78cc8d07dfc531ed52ba690ce28bab2c859a4679
Wator Vapor <watorvapor@gmail.com>
2018-03-09 22:22:59 +0900

    Create ttsmaster.hpp

---
+++ watorword/ttsmaster.hpp
@@ -0,0 +1,19 @@
+#include <string>
+using namespace std;
+#pragma once
+
+#include <leveldb/db.h>
+
+class TTSMaster {
+ public:
+  TTSMaster(const string &path);
+  ~TTSMaster();
+  void openDB();
+  void closeDB();
+  string getDoublePred(const string &word);
+
+ private:
+ private:
+  string master_db_path_;
+  leveldb::DB *master_;
+};
