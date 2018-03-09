82ca3dbecebe2eeabbab792b21b913873210b1e1
Wator Vapor <watorvapor@gmail.com>
2018-03-09 23:14:36 +0900

    Update zhiziword.tts.cpp

--- watorword/zhiziword.tts.cpp
+++ watorword/zhiziword.tts.cpp
@@ -18,12 +18,40 @@ using namespace std;
 static const TTSMaster gCN("./db/tts/cn");
 static const TTSMaster gJA("./db/tts/ja");
 
+vector<string> parseUTF8(const string &words);
+
 vector<string> ZhiZiWord::createAudioList(const string &lang) {
   vector<string> result;
   DUMP_VAR(lang);
   for(auto wordSed:wordSeqTopSelected_) {
     auto word = std::get<0>(wordSed.second);
     DUMP_VAR(word);
+    auto phoneme = createPhoneme(word,lang);
+    result.insert(result.end(),phoneme);
   }
   return result;
+}
+
+#include <boost/algorithm/string.hpp>
+
+vector<string> ZhiZiWord::createPhoneme(const string &word,const string &lang) {
+  vector<string> result;
+  if(lang == "cn") {
+    auto phrasePinyin = gCN.getPhrase(word);
+    if(phrasePinyin.empty()) {
+      auto hanziA = parseUTF8(word);
+      for(auto hanzi :hanziA) {
+        auto pinyin = gCN.getHanzi(hanzi);
+        if(pinyin.empty() == false) {
+          result.push_back(pinyin);
+        } else {
+          result.push_back("miss hanzi");
+        }
+      }
+    } else {
+      std::vector<std::string> results;
+      boost::algorithm::split(results, phrasePinyin, is_any_of(","));
+    }
+  }
+  result.push_back("word space")
 }
