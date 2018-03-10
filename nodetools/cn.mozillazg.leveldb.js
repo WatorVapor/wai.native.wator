5ee2b8801e8af559beffca1b0c9ccbde6d36376c
Wator Vapor <watorvapor@gmail.com>
2018-03-10 12:13:34 +0900

    Update cn.mozillazg.leveldb.js

--- nodetools/cn.mozillazg.leveldb.js
+++ nodetools/cn.mozillazg.leveldb.js
@@ -15,21 +15,24 @@ pinYinLine.forEach(function(line) {
     //console.log('line =<',line,'>');
     let param1 = line.split(':');
     //console.log('param1[1] =<',param1[1],'>');
-    let param2 = param1[1].split('#');
-    //console.log('param2[0] =<',param2[0],'>');
-    //console.log('param2[1] =<',param2[1],'>');
-    let hanzi = param2[1].trim()
-    let param3 = param2[0].split(',');
-    let pinYin = [];
-    param3.forEach(function(pinYinRaw) {
-      pinYin.push(pinYinRaw.trim());
-    });
-    let pinYinGood = pinYin.join(',');
-    console.log('hanzi =<',hanzi,'>');
-    console.log('pinYinGood =<',pinYinGood,'>');
-    dist.put(hanzi,pinYinGood);
+    if(param1.length > 1) {
+      let param2 = param1[1].split('#');
+      //console.log('param2[0] =<',param2[0],'>');
+      //console.log('param2[1] =<',param2[1],'>');
+      let hanzi = param2[1].trim()
+      let param3 = param2[0].split(',');
+      let pinYin = [];
+      param3.forEach(function(pinYinRaw) {
+        pinYin.push(pinYinRaw.trim());
+      });
+      let pinYinGood = pinYin.join(',');
+      //console.log('hanzi =<',hanzi,'>');
+      //console.log('pinYinGood =<',pinYinGood,'>');
+      dist.put(hanzi,pinYinGood);
+    } else {
+      console.log('param1 =<',param1,'>');
+    }
   }
 });
 
-console.log('phonemeDB =<',phonemeDB,'>');
 
