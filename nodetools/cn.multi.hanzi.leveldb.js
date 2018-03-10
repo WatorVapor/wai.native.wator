85790e7b0588a9f510d4674a70a81b861158f9fa
Wator Vapor <watorvapor@gmail.com>
2018-03-10 11:15:14 +0900

    Update cn.multi.hanzi.leveldb.js

--- nodetools/cn.multi.hanzi.leveldb.js
+++ nodetools/cn.multi.hanzi.leveldb.js
@@ -1,9 +1,7 @@
 var levelup = require('levelup');
 var phonemePath = '/watorvapor/rsaauth/wator/pubKeys/b3f3e38d032b4b956d3c4591f40aaff21fb337ec1340626eaf3b35e84e23ced763c18502925f0a16d5b4a10284b83d5e15cbc1cbcec593e095c0776d287e8f36/wai/phoneme_cn.json';
+var phonemeDB = require(phonemePath);
 
-
-//var distDB = args.targets[1];
-//console.log('distDB =<',distDB,'>');
 var hanziDB = {};
 if(phonemeDB.cn) {
   phonemeDB.cn.forEach(function(phoneme){
