8dbf4b8b0e39ff23ddd86a0063dd7eca1cab1cfd
Wator Vapor <watorvapor@gmail.com>
2018-03-10 12:06:58 +0900

    Update cn.mozillazg.leveldb.js

--- nodetools/cn.mozillazg.leveldb.js
+++ nodetools/cn.mozillazg.leveldb.js
@@ -1,5 +1,7 @@
 var levelup = require('levelup');
 
+var phonemeDB = {};
+
 var fs = require('fs');
 var dataPinYin = fs.readFileSync('pinyin.txt', {
   encoding: 'utf-8'
@@ -11,14 +13,22 @@ pinYinLine.forEach(function(line) {
   if(!line.startsWith('#')) {
     //console.log('line =<',line,'>');
     let param1 = line.split(':');
-    console.log('param1[1] =<',param1[1],'>');
+    //console.log('param1[1] =<',param1[1],'>');
     let param2 = param1[1].split('#');
-    console.log('param2[0] =<',param2[0],'>');
-    console.log('param2[1] =<',param2[1],'>');
+    //console.log('param2[0] =<',param2[0],'>');
+    //console.log('param2[1] =<',param2[1],'>');
+    let hanzi = param2[1].trim()
+    let param3 = param2[0].split(',');
+    let pinYin = [];
+    param3.forEach(function(pinYinRaw) {
+      pinYin.push(pinYinRaw.trim());
+    });
+    let pinYinGood = JSON.stringify(pinYin);
+    console.log('hanzi =<',hanzi,'>');
+    console.log('pinYinGood =<',pinYinGood,'>');
   }
 });
 
-var phonemeDB = {};
 console.log('phonemeDB =<',phonemeDB,'>');
 
 var distDB = '../db/tts/cn/hanzi'
