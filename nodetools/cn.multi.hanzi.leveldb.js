32971c8bff75ce9175a24b6a2fec10823780e4b3
Wator Vapor <watorvapor@gmail.com>
2018-03-10 11:14:24 +0900

    Update cn.multi.hanzi.leveldb.js

--- nodetools/cn.multi.hanzi.leveldb.js
+++ nodetools/cn.multi.hanzi.leveldb.js
@@ -1,1 +1,36 @@
+var levelup = require('levelup');
+var phonemePath = '/watorvapor/rsaauth/wator/pubKeys/b3f3e38d032b4b956d3c4591f40aaff21fb337ec1340626eaf3b35e84e23ced763c18502925f0a16d5b4a10284b83d5e15cbc1cbcec593e095c0776d287e8f36/wai/phoneme_cn.json';
+
+
+//var distDB = args.targets[1];
+//console.log('distDB =<',distDB,'>');
+var hanziDB = {};
+if(phonemeDB.cn) {
+  phonemeDB.cn.forEach(function(phoneme){
+    //console.log('phoneme =<',phoneme,'>');
+    let hanzis = phoneme.cn_help.split(' ');
+    //console.log('hanzis =<',hanzis,'>');
+    hanzis.forEach(function(hanzi){
+      //console.log('hanzi =<',hanzi,'>');
+      tryAddHanzi(hanzi,phoneme.phoneme);
+    });
+  });
+}
+
+var multiHanziDB = {};
+
+function tryAddHanzi(hanzi,phoneme) {
+  if(! hanziDB[hanzi]) {
+    hanziDB[hanzi] = phoneme;
+  } else {
+    hanziDB[hanzi] += ',' + phoneme;
+  }
+  
+  if(! multiHanziDB[hanzi]) {
+    multiHanziDB[hanzi] = [phoneme];
+  } else {
+    multiHanziDB[hanzi].push(phoneme);
+  }
+}
+console.log('multiHanziDB =<',multiHanziDB,'>');
 
