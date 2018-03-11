84886df7b5daf2ba664b3f1b4b8030b598837f99
Wator Vapor <watorvapor@gmail.com>
2018-03-11 18:41:56 +0900

    Update zhiziword.tts.cpp

--- watorword/zhiziword.tts.cpp
+++ watorword/zhiziword.tts.cpp
@@ -41,6 +41,17 @@ vector<string> ZhiZiWord::createAudioList(const string &lang) {
       }
     }
   }
+  
+  if(lang == "ja") {
+    for(auto phoneme:phonemeList) {
+      auto clipAudio = gJA.getPhoneme(phoneme);
+      if(clipAudio.empty() == false) {
+        result.push_back(clipAudio);
+      } else {
+        DUMP_VAR2(phoneme,clipAudio);
+      }
+    }
+  }
   return result;
 }
 
