795615e90aadb27e708068ba96995efffe1fef44
Wator Vapor <watorvapor@gmail.com>
2018-03-11 11:47:22 +0900

    Update ja.hirakana.leveldb.js

--- nodetools/ja.hirakana.leveldb.js
+++ nodetools/ja.hirakana.leveldb.js
@@ -1,1 +1,17 @@
+var levelup = require('levelup');
+
+var distDB = '../db/tts/ja/hanzi'
+var dist = levelup(distDB);
+
+var Kana = {
+  あ:'あ',
+};
+
+
+Kana.forEach(function(Kana,phoneme){
+  console.log('Kana =<',Kana,'>');
+  console.log('phoneme =<',phoneme,'>');
+  dist.put(Kana,phoneme);
+});
+
 
