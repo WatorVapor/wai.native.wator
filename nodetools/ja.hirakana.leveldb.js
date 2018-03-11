7aa873f462d75e768a3e687b614dc0a0f7c3857d
Wator Vapor <watorvapor@gmail.com>
2018-03-11 11:51:42 +0900

    Update ja.hirakana.leveldb.js

--- nodetools/ja.hirakana.leveldb.js
+++ nodetools/ja.hirakana.leveldb.js
@@ -3,15 +3,15 @@ var levelup = require('levelup');
 var distDB = '../db/tts/ja/hanzi'
 var dist = levelup(distDB);
 
-var Kana = [
-  {あ:'あ'}
-];
-console.log('Kana =<',Kana,'>');
+var phonemePath = '/watorvapor/rsaauth/wator/pubKeys/b3f3e38d032b4b956d3c4591f40aaff21fb337ec1340626eaf3b35e84e23ced763c18502925f0a16d5b4a10284b83d5e15cbc1cbcec593e095c0776d287e8f36/wai/phoneme_ja.json';
+var phonemeDB = require(phonemePath);
+console.log('phonemeDB =<',phonemeDB,'>');
 
+/*
 Kana.forEach(function(jakan,phoneme){
   console.log('jakan =<',jakan,'>');
   console.log('phoneme =<',phoneme,'>');
   dist.put(jakan,phoneme);
 });
-
+*/
 
