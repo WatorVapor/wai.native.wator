06a656fb0c80ae4fa855804040ff51092dd8cbe6
Wator Vapor <watorvapor@gmail.com>
2018-03-14 21:54:38 +0900

    Update wordFromLevelDB.js

--- wiktionary/wordFromLevelDB.js
+++ wiktionary/wordFromLevelDB.js
@@ -1,1 +1,22 @@
-var levelup = require('levelup');
+let levelup = require('levelup');
+module.exports = class WordDBUtility {
+  constructor(option) {
+    if(option.input) {
+      this.input = levelup(option.input);
+    }
+    if(option.output) {
+      this.output = levelup(option.output);
+    }
+    this.dry = false;
+    if(option.dry) {
+      this.dry = option.dry;
+    }
+    console.log('constructor::this.input=<',this.input,'>');
+    console.log('constructor::this.output=<',this.output,'>');
+  }
+    
+  getWord(cb){
+    this.cb = cb;
+    console.log('readKana_::kana1=<',kana1,'>');
+  }
+}
