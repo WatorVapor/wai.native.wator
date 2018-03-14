2e7a05f0d96f2a5b7f9611f58a5252a587951288
Wator Vapor <watorvapor@gmail.com>
2018-03-15 06:55:15 +0900

    Update wordFromLevelDB.js

--- wiktionary/wordFromLevelDB.js
+++ wiktionary/wordFromLevelDB.js
@@ -28,12 +28,35 @@ module.exports = class WordDBUtility {
         console.log('constructor::e=<',e,'>');
       }
     }
-    this.dry = false;
+     if(option.done) {
+      try {
+        //console.log('constructor::option.done=<',option.done,'>');
+        this.done = level(option.done);
+        //console.log('constructor::this.done=<',this.done,'>');
+        let self= this;
+        this.done.get('lastDone', function (err, value) {
+          if(err) {
+          } else {
+            self.lastDone = value.toString('utf8');
+          }
+        });
+      } catch(e) {
+        console.log('constructor::e=<',e,'>');
+      }
+    }
+   this.dry = false;
     if(option.dry) {
       this.dry = option.dry;
     }
     //console.log('constructor::this.input=<',this.input,'>');
     //console.log('constructor::this.output=<',this.output,'>');
+    //console.log('constructor::this.done=<',this.done,'>');
+    let timer = setInterval(function(){
+      if(this.inputIt && this.lastDone) {
+        this.inputIt.seek(this.lastDone);
+        clearInterval(timer);
+      }
+    }.bind(this),1000)
   }
     
   getWord(cb){
@@ -42,7 +65,7 @@ module.exports = class WordDBUtility {
     if(!this.inputIt) {
       return 'prepare';
     }
-    ///let self = this;
+    let self = this;
     this.inputIt.next(function(error,key,value){
       if(error) {
         console.log('getWord::error=<',error,'>');
@@ -52,8 +75,13 @@ module.exports = class WordDBUtility {
         //console.log('getWord::key.toString=<',key.toString('utf8'),'>');
         //console.log('getWord::value=<',value,'>');
         cb(key.toString('utf8'));
+        self.prev = key.toString('utf8');
       }
     });
+    if(this.prev) {
+      this.done.put(this.prev,'true');
+      this.done.put('lastDone',this.prev);
+    }
     return 'ready';
   }
   setWordKana(word,kana) {
