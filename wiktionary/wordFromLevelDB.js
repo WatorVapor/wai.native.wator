13765d0a896b02402df4032382df16a9e5ae875f
Wator Vapor <watorvapor@gmail.com>
2018-03-14 23:35:47 +0900

    Update wordFromLevelDB.js

--- wiktionary/wordFromLevelDB.js
+++ wiktionary/wordFromLevelDB.js
@@ -8,7 +8,10 @@ module.exports = class WordDBUtility {
       //console.log('constructor::option.input=<',option.input,'>');
       this.input = LevelDown(option.input);
       console.log('constructor::this.input=<',this.input,'>');
-      this.inputIt = this.input.iterator();
+      this.input.open(function(error){
+        console.log('constructor::error=<',error,'>');
+      });
+      //this.inputIt = this.input.iterator();
       //console.log('constructor::this.inputIt=<',this.inputIt,'>');
     }
     if(option.output) {
