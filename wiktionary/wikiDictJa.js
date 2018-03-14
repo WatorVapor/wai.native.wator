2df86bda34b1666130ca7336f2b25a64f4b93249
Wator Vapor <watorvapor@gmail.com>
2018-03-14 23:30:57 +0900

    Update wikiDictJa.js

--- wiktionary/wikiDictJa.js
+++ wiktionary/wikiDictJa.js
@@ -16,7 +16,7 @@ module.exports = class WikiDict {
       input:'../../input_db/zhizi/ja',
       output:'../../out_db/ja',
     };
-    this.dictDB = new wordDB(this.db);
+    //this.dictDB = new wordDB(this.db);
   }
     
   runOnce(cb){
