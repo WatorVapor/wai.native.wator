550959b926497ba87215881d5cd252708ba3a4d8
Wator Vapor <watorvapor@gmail.com>
2018-04-10 12:48:58 +0900

    Create zhwiktionary.js

---
+++ ipfs-wikidump/zhwiktionary.js
@@ -0,0 +1,8 @@
+const wiki = require('./parseWikiDumper.js');
+let dumpPath = '/watorvapor/wai.storage/dumps.wikimedia.org/zhwiktionary/zhwiktionary-20180401-pages-articles.xml';
+let wikiDumper = new wiki(dumpPath,onPage);
+
+function onPage(title,text){
+  console.log('onPage::title=<',title,'>');
+  //console.log('onPage::text=<',text,'>');
+}
