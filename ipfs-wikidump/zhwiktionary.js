2cc719a8e1357655b35541be7f55fa623e3d8f98
Wator Vapor <watorvapor@gmail.com>
2018-04-10 13:18:31 +0900

    Update zhwiktionary.js

--- ipfs-wikidump/zhwiktionary.js
+++ ipfs-wikidump/zhwiktionary.js
@@ -8,13 +8,13 @@ function onPage(title,text){
   fetchByPronPinYin(title,text);
 }
 function fetchByPronPinYin(title,textPure) {
-  //console.log('fetchByPronPinYin::title=<',title,'>');
+  console.log('fetchByPronPinYin::title=<',title,'>');
   let parma1 = textPure.split('{{漢語讀音');
   if(parma1.length > 1) {
     //console.log('fetchByPronPinYin::parma1[1]=<',parma1[1],'>');
     let parma2 = parma1[1].split('}}');
     if(parma2.length > 1) {
-      console.log('fetchByPronPinYin::parma2[0]=<',parma2[0],'>');
+      //console.log('fetchByPronPinYin::parma2[0]=<',parma2[0],'>');
       let parma3 = parma2[0].split('漢語拼音=')
       if(parma3.length > 1) {
         console.log('fetchByPronPinYin::parma3[1]=<',parma3[1],'>');
