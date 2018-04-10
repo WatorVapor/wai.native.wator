9ca7260bc2261cde040e3c15b63f70dea870f94f
Wator Vapor <watorvapor@gmail.com>
2018-04-10 15:28:24 +0900

    Update zhwiktionary.js

--- ipfs-wikidump/zhwiktionary.js
+++ ipfs-wikidump/zhwiktionary.js
@@ -36,7 +36,7 @@ function fetchByPronPinYin(title,textPure) {
           }
           console.log('fetchByPronPinYin::title=<',title,'>');
           console.log('fetchByPronPinYin::pinYin2=<',pinYin2,'>');
-          console.log('fetchByPronPinYin::title.length=<',title.length,'>');
+          //console.log('fetchByPronPinYin::title.length=<',title.length,'>');
           //console.log('fetchByPronPinYin::parma3=<',parma3,'>');
         }
       }
