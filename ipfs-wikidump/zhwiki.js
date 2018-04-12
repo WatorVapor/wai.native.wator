ba19a60dd4711ef95e1d16c74d8d6cd93d1f5b11
Wator Vapor <watorvapor@gmail.com>
2018-04-12 20:56:39 +0900

    Update zhwiki.js

--- ipfs-wikidump/zhwiki.js
+++ ipfs-wikidump/zhwiki.js
@@ -14,8 +14,63 @@ function pushToDB(title,page) {
 }
 
 function onPage(title,text){
+  let filters = [
+    'Wikipedia:','Help:',
+  ];
+  if(filterTitle(filters,title)) {
+    //console.log('onPage::filter out title=<',title,'>');
+    return;
+  }
+  if(filterAscii(title)) {
+    //console.log('onPage::filter out title=<',title,'>');
+    return;
+  }
+  if(filterMaoHao(title)) {
+    //console.log('onPage::filter out title=<',title,'>');
+    return;
+  }
   console.log('onPage::title=<',title,'>');
   //console.log('onPage::text=<',text,'>');
+}
+
+
+
+
+
+
+
+
+
+
+
+function filterTitle(filters,title) {
+  for(let i = 0;i < filters.length;i++) {
+    let filter = filters[i];
+    if(title.includes(filter)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function filterAscii(title) {
+  for(let i = 0;i < title.length;i++) {
+    let charCode = title.charAt(i);
+    //console.log('filterAccii:charCode=<',charCode,'>');
+    if(charCode >= '、') {
+      return false;
+    }
+  }
+  return true;
+}
+function filterMaoHao(title) {
+  if(title.includes(':')) {
+    return true;
+  }
+  if(title.includes('：')) {
+    return true;
+  }
+  return false;
 }
 
 
