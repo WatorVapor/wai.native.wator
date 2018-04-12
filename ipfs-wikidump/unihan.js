8752fa8b4bd3160c3bfb82f2853bb09907bbd5bc
Wator Vapor <watorvapor@gmail.com>
2018-04-12 12:33:04 +0900

    Update unihan.js

--- ipfs-wikidump/unihan.js
+++ ipfs-wikidump/unihan.js
@@ -1,2 +1,16 @@
 const fs = require('fs')
 const cheerio = require('cheerio');
+
+const unihanPath = '/watorvapor/wai.storage/www.unicode.org/ucd.unihan.flat.xml';
+let xml_data = fs.readFileSync(unihanPath, "utf-8");
+const $ = cheerio.load(xml_data);
+
+$('description').each( (i, elem) => {
+  let textElem = elem.children[0];
+  if(textElem.type === 'text') {
+    let length = textElem.data.length;
+    let description = textElem.data.substr(0,length);
+    console.log('description=<',description,'>');
+  }
+});
+
