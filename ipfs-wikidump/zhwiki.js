c4752485045b4adafeb657815f76067bad4d99d1
Wator Vapor <watorvapor@gmail.com>
2018-04-16 22:05:26 +0900

    Update zhwiki.js

--- ipfs-wikidump/zhwiki.js
+++ ipfs-wikidump/zhwiki.js
@@ -41,7 +41,7 @@ function onPage(title,pos,text){
   }
   let filters = [
     'Wikipedia:','Help:','Template:','Category:','MediaWiki:','Hex',
-    'File:','Portal:'
+    'File:','Portal:','模块:',
   ];
   if(filterTitle(filters,title)) {
     //console.log('onPage::filter out title=<',title,'>');
