0431a8de6537d24b84997c841e82c90328b7b3ec
Wator Vapor <watorvapor@gmail.com>
2018-04-16 22:40:49 +0900

    Create unihan_ja_roma.js

---
+++ ipfs-wikidump/unihan_ja_roma.js
@@ -0,0 +1,315 @@
+{
+    "roma": [
+        {
+            "a": "あ",
+        },
+        {
+            "phoneme": "い",
+        },
+        {
+            "phoneme": "う",
+        },
+        {
+            "phoneme": "え",
+        },
+        {
+            "phoneme": "お",
+        },
+        {
+            "phoneme": "か",
+        },
+        {
+            "phoneme": "き",
+        },
+        {
+            "phoneme": "く",
+        },
+        {
+            "phoneme": "け",
+        },
+        {
+            "phoneme": "こ",
+        },
+        {
+            "phoneme": "が",
+        },
+        {
+            "phoneme": "ぎ",
+        {
+            "phoneme": "ぐ",
+        },
+        {
+            "phoneme": "げ",
+        },
+        {
+            "phoneme": "ご",
+        },
+        {
+            "phoneme": "さ",
+        },
+        {
+            "phoneme": "し",
+        },
+        {
+            "phoneme": "す",
+        },
+        {
+            "phoneme": "せ",
+        },
+        {
+            "phoneme": "そ",
+        },
+        {
+            "phoneme": "ざ",
+        },
+        {
+            "phoneme": "じ",
+        },
+        {
+            "phoneme": "ず",
+        },
+        {
+            "phoneme": "ぜ",
+        },
+        {
+            "phoneme": "ぞ",
+        },
+        {
+            "phoneme": "た",
+        },
+        {
+            "phoneme": "ち",
+        },
+        {
+            "phoneme": "つ",
+        },
+        {
+            "phoneme": "て",
+        },
+        {
+            "phoneme": "と",
+        },
+        {
+            "phoneme": "だ",
+        },
+        {
+            "phoneme": "ぢ",
+        },
+        {
+            "phoneme": "づ",
+        },
+        {
+            "phoneme": "で",
+        },
+        {
+            "phoneme": "ど",
+        },
+        {
+            "phoneme": "な",
+        },
+        {
+            "phoneme": "に",
+        },
+        {
+            "phoneme": "ぬ",
+        },
+        {
+            "phoneme": "ね",
+        },
+        {
+            "phoneme": "の",
+        },
+        {
+            "phoneme": "は",
+        },
+        {
+            "phoneme": "ひ",
+        },
+        {
+            "phoneme": "ふ",
+        },
+        {
+            "phoneme": "へ",
+        },
+        {
+            "phoneme": "ほ",
+        },
+        {
+            "phoneme": "ば",
+        },
+        {
+            "phoneme": "び",
+        },
+        {
+            "phoneme": "ぶ",
+        },
+        {
+            "phoneme": "べ",
+        },
+        {
+            "phoneme": "ぼ",
+        },
+        {
+            "phoneme": "ぱ",
+        },
+        {
+            "phoneme": "ぴ",
+        },
+        {
+            "phoneme": "ぷ",
+        },
+        {
+            "phoneme": "ぺ",
+        },
+        {
+            "phoneme": "ぽ",
+        },
+        {
+            "phoneme": "ま",
+        },
+        {
+            "phoneme": "み",
+        },
+        {
+            "phoneme": "む",
+        },
+        {
+            "phoneme": "め",
+        },
+        {
+            "phoneme": "も",
+        },
+        {
+            "phoneme": "や",
+        },
+        {
+            "phoneme": "ゆ",
+        },
+        {
+            "phoneme": "よ",
+        },
+        {
+            "phoneme": "ら",
+        },
+        {
+            "phoneme": "り",
+        },
+        {
+            "phoneme": "る",
+        },
+        {
+            "phoneme": "れ",
+        },
+        {
+            "phoneme": "ろ",
+        },
+        {
+            "phoneme": "わ",
+        },
+        {
+            "phoneme": "を",
+        },
+        {
+            "phoneme": "ん",
+        },
+        {
+            "phoneme": "きゃ",
+        },
+        {
+            "phoneme": "きゅ",
+        },
+        {
+            "phoneme": "きょ",
+        },
+        {
+            "phoneme": "ぎゃ",
+        },
+        {
+            "phoneme": "ぎゅ",
+        },
+        {
+            "phoneme": "ぎょ",
+        },
+        {
+            "phoneme": "しゃ",
+        },
+        {
+            "phoneme": "しゅ",
+        },
+        {
+            "phoneme": "しょ",
+        },
+        {
+            "phoneme": "じゃ",
+        },
+        {
+            "phoneme": "じゅ",
+        },
+        {
+            "phoneme": "じょ",
+        },
+        {
+            "phoneme": "ちゃ",
+        },
+        {
+            "phoneme": "ちゅ",
+        },
+        {
+            "phoneme": "ちょ",
+        },
+        {
+            "phoneme": "にゃ",
+        },
+        {
+            "phoneme": "にゅ",
+        },
+        {
+            "phoneme": "にょ",
+        },
+        {
+            "phoneme": "ひゃ",
+        },
+        {
+            "phoneme": "ひゅ",
+        },
+        {
+            "phoneme": "ひょ",
+        },
+        {
+            "phoneme": "びゃ",
+        },
+        {
+            "phoneme": "びゅ",
+        },
+        {
+            "phoneme": "びょ",
+        },
+        {
+            "phoneme": "ぴゃ",
+        },
+        {
+            "phoneme": "ぴゅ",
+        },
+        {
+            "phoneme": "ぴょ",
+        },
+        {
+            "phoneme": "みゃ",
+        },
+        {
+            "phoneme": "みゅ",
+        },
+        {
+            "phoneme": "みょ",
+        },
+        {
+            "phoneme": "りゃ",
+        },
+        {
+            "phoneme": "りゅ",
+        },
+        {
+            "phoneme": "りょ",
+        }
+    ]
+}
