a916b0cc38d14af87031242184b228f0715750df
Wator Vapor <watorvapor@gmail.com>
2018-05-02 22:46:32 +0900

    Update ipfs-cnwiki.js

--- ipfs-wikidump/ipfs-cnwiki.js
+++ ipfs-wikidump/ipfs-cnwiki.js
@@ -3,15 +3,14 @@ let dbPathTitle = '/watorvapor/wai.storage/dumps.wikimedia.org/output_leveldb/cn
 var dbTitle = level(dbPathTitle);
 var stream = dbTitle.createReadStream();
 
-let dbPathPage = '/watorvapor/wai.storage/dumps.wikimedia.org/output_leveldb/cnwiki/page';
-var dbPage = level(dbPathPage);
 
 
 var counter = 0;
 stream.on('data', function (data) {
   //console.log(data.key.toString('utf-8'), '=', data.value.toString('utf-8'));
-  console.log('data.key=<',data.key.toString('utf-8'),'>')
-  console.log('data.value=<',data.value.toString('utf-8'),'>')
+  let title = data.key.toString('utf-8');
+  let keyPage = data.value.toString('utf-8');
+  GetPage(title,keyPage);
   counter++
 });
 stream.on('error', function (err) {
@@ -24,3 +23,18 @@ stream.on('end', function () {
   console.log('Stream ended')
   console.log('counter=<',counter,'>')
 });
+
+let dbPathPage = '/watorvapor/wai.storage/dumps.wikimedia.org/output_leveldb/cnwiki/page';
+var dbPage = level(dbPathPage);
+
+function GetPage(title,key) {
+  console.log('GetPage::title=<',title,'>');
+  console.log('GetPage::key=<',key,'>');
+  dbPage.get(key, function (err, value) {
+    if (err) {
+      console.log('GetPage::err=<',err,'>')
+      return;
+    }
+    console.log('GetPage::value=<',value,'>');
+  });
+}
