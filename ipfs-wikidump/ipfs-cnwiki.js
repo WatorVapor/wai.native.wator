9357f7c312aaddffaaf3d8436f63463ad554f2ad
Wator Vapor <watorvapor@gmail.com>
2018-05-04 08:13:42 +0900

    cnwiki.js

--- ipfs-wikidump/ipfs-cnwiki.js
+++ ipfs-wikidump/ipfs-cnwiki.js
@@ -1,40 +1,153 @@
+const dumpPath = '/watorvapor/wai.storage/dumps.wikimedia.org/zhwiki/zhwiki-20180420-pages-articles.xml';
+const dbTitlePath = '/watorvapor/wai.storage/dumps.wikimedia.org/output_leveldb/cnwiki/title';
+const dbPagePath = '/watorvapor/wai.storage/dumps.wikimedia.org/output_leveldb/cnwiki/page';
+
+let skipTitles = [
+  'Wikipedia:','Help:','Template:','Category:','MediaWiki:','Hex',
+  'File:','Portal:','模块:',
+];
+
+const wiki = require('./parseWikiDumper.js');
 const level = require('level');
-let dbPathTitle = '/watorvapor/wai.storage/dumps.wikimedia.org/output_leveldb/cnwiki/title';
-var dbTitle = level(dbPathTitle);
-var stream = dbTitle.createReadStream();
+let dbTittle = level(dbTitlePath);
+let dbPage = level(dbPagePath);
+const SHA3 = require('sha3');
+const execSync = require('child_process').execSync;
+const fs = require('fs');
 
 
 
-var counter = 0;
-stream.on('data', function (data) {
-  //console.log(data.key.toString('utf-8'), '=', data.value.toString('utf-8'));
-  let title = data.key.toString('utf-8');
-  let keyPage = data.value.toString('utf-8');
-  GetPage(title,keyPage);
-  counter++
-});
-stream.on('error', function (err) {
-  console.log('Oh my!', err)
-});
-stream.on('close', function () {
-  console.log('Stream closed')
-});
-stream.on('end', function () {
-  console.log('Stream ended')
-  console.log('counter=<',counter,'>')
-});
+function pushTitle2DB(key,value) {
+  //console.log('pushToDB::key=<',key,'>');
+  //console.log('pushToDB::value=<',value,'>');
+  dbTittle.put(key,value);
+}
 
-let dbPathPage = '/watorvapor/wai.storage/dumps.wikimedia.org/output_leveldb/cnwiki/page';
-var dbPage = level(dbPathPage);
-
-async function GetPage(title,key) {
-  await dbPage.get(key, function (err, value) {
+function pushPage2DB(key,value,cnTitle,zhTitle) {
+  //console.log('pushPage2DB::key=<',key,'>');
+  //console.log('pushPage2DB::value=<',value,'>');
+  dbPage.get(key,function (err, valueOld) {
     if (err) {
-      console.log('GetPage::err=<',err,'>')
-      return;
+      if (err.notFound) {
+        //console.log('pushPage2DB:: new cnTitle=<',cnTitle,'>');
+        dbPage.put(key,value);
+      }
+    } else {
+      //console.log('pushPage2DB::cnTitle=<',cnTitle,'>');
+      //console.log('pushPage2DB::zhTitle=<',zhTitle,'>');
+      //console.log('pushPage2DB::valueOld=<',valueOld.length,'>');
+      //console.log('pushPage2DB::value=<',value.length,'>');
+      if(value.length > valueOld.length) {
+        dbPage.put(key,value);
+      }
     }
-    console.log('GetPage::title=<',title,'>');
-    console.log('GetPage::key=<',key,'>');
-    console.log('GetPage::value=<',value,'>');
   });
 }
+
+const dbTitblePagePath = '/watorvapor/wai.storage/dumps.wikimedia.org/output_leveldb/cnwiki/titlePage';
+let dbTitlePage = level(dbTitblePagePath);
+
+async function pushTitlePage2DB(key,value,cnTitle,zhTitle) {
+  //console.log('pushPage2DB::key=<',key,'>');
+  //console.log('pushPage2DB::value=<',value,'>');
+  await dbTitlePage.get(key,function (err, valueOld) {
+    if (err) {
+      if (err.notFound) {
+        //console.log('pushPage2DB:: new cnTitle=<',cnTitle,'>');
+        dbTitlePage.put(key,value);
+      }
+    } else {
+      //console.log('pushPage2DB::cnTitle=<',cnTitle,'>');
+      //console.log('pushPage2DB::zhTitle=<',zhTitle,'>');
+      //console.log('pushPage2DB::valueOld=<',valueOld.length,'>');
+      //console.log('pushPage2DB::value=<',value.length,'>');
+      if(value.length > valueOld.length) {
+        dbTitlePage.put(key,value);
+      }
+    }
+  });
+}
+
+function pushPosTitlePage2DB(key,value) {
+  //console.log('pushToDB::key=<',key,'>');
+  //console.log('pushToDB::value=<',value,'>');
+  dbTitlePage.put(key,value);
+}
+
+
+
+const ResumePosKey = 'wiki_dump_resume_pos';
+let ResumePos = 0;
+dbTitlePage.get(ResumePosKey, function (err, value) {
+  if (!err) {
+    console.log('db.get::value=<',value,'>');
+    ResumePos = parseInt(value);
+    if(ResumePos > 1024) {
+      ResumePos -= 1024;
+    }
+  }
+  setTimeout(function(){
+    let wikiDumper = new wiki(dumpPath,ResumePos,onPage);
+  },1);
+})
+
+
+const opencc = require('node-opencc');
+
+function onPage(zhTitle,pos,zhText){
+  if(!zhTitle) {
+    return;
+  }
+  if(filterTitle(skipTitles,zhTitle)) {
+    //console.log('onPage::filter out zhTitle=<',zhTitle,'>');
+    return;
+  }
+  //console.log('onPage::zhTitle=<',zhTitle,'>');
+  let cnTitle = opencc.traditionalToSimplified(zhTitle);
+  let cnText = opencc.traditionalToSimplified(zhText);
+  //console.log('onPage::cnText=<',cnText,'>');
+  pushTitlePage2DB(cnTitle,cnText,cnTitle,zhTitle);
+  pushPosTitlePage2DB(ResumePosKey,pos);
+}
+
+
+
+
+
+
+
+
+
+
+
+function filterTitle(filters,title) {
+  for(let i = 0;i < filters.length;i++) {
+    let filter = filters[i];
+    if(title.includes(filter)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function filterAscii(title) {
+  for(let i = 0;i < title.length;i++) {
+    let charCode = title.charAt(i);
+    //console.log('filterAccii:charCode=<',charCode,'>');
+    if(charCode >= '、') {
+      return false;
+    }
+  }
+  return true;
+}
+function filterMaoHao(title) {
+  if(title.includes(':')) {
+    return true;
+  }
+  if(title.includes('：')) {
+    return true;
+  }
+  return false;
+}
+
+
