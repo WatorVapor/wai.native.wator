b9b7148ea954164b4bf79178dc2f476676520256
Wator Vapor <watorvapor@gmail.com>
2018-05-06 08:05:43 +0900

    Update ipfs-jawiki.js

--- ipfs-wikidump/ipfs-jawiki.js
+++ ipfs-wikidump/ipfs-jawiki.js
@@ -1,7 +1,5 @@
 const dumpPath = '/watorvapor/wai.storage/dumps.wikimedia.org/jawiki/jawiki-20180420-pages-articles.xml';
-const dbTitlePath = '/watorvapor/wai.storage/dumps.wikimedia.org/output_leveldb/jawiki/title';
-const dbPagePath = '/watorvapor/wai.storage/dumps.wikimedia.org/output_leveldb/jawiki/page';
-
+const dbPath = '/watorvapor/wai.storage/dumps.wikimedia.org/output_leveldb/jawiki/ipfs';
 const skipTitles = [
     'Wikipedia:','Help:','Template:','Category:','MediaWiki:','Hex',
     'HEX','File:','Portal:','ファイル:'
@@ -10,30 +8,43 @@ const skipTitles = [
 
 const wiki = require('./parseWikiDumper.js');
 const level = require('level');
-let dbTittle = level(dbTitlePath);
-let dbPage = level(dbPagePath);
-const SHA3 = require('sha3');
-const execSync = require('child_process').execSync;
-const fs = require('fs');
+const ipfsAPI = require('ipfs-api');
+const ipfs = ipfsAPI({host: 'localhost', port: '5002', protocol: 'http'});
 
 
 
-function pushTitle2DB(key,value) {
-  //console.log('pushToDB::key=<',key,'>');
-  //console.log('pushToDB::value=<',value,'>');
-  dbTittle.put(key,value);
+ipfs.id(function (err, identity) {
+  if (err) {
+    throw err;
+  }
+  console.log('ipfs.id identity=<',identity,'>');
+});
+
+
+
+
+
+let db = level(dbPath);
+let wikiDumper = false;
+
+
+async function pushIpfs2DB(key,value) {
+  //console.log('pushPage2DB::key=<',key,'>');
+  //console.log('pushPage2DB::value=<',value,'>');
+  db.put(key,value);
 }
 
-function pushPage2DB(key,value) {
+function pushPosIpfs2DB(key,value) {
   //console.log('pushToDB::key=<',key,'>');
   //console.log('pushToDB::value=<',value,'>');
-  dbPage.put(key,value);
+  db.put(key,value);
 }
+
 
 
 const ResumePosKey = 'wiki_dump_resume_pos';
 let ResumePos = 0;
-dbTittle.get(ResumePosKey, function (err, value) {
+db.get(ResumePosKey, function (err, value) {
   if (!err) {
     console.log('db.get::value=<',value,'>');
     ResumePos = parseInt(value);
@@ -42,31 +53,53 @@ dbTittle.get(ResumePosKey, function (err, value) {
     }
   }
   setTimeout(function(){
-    let wikiDumper = new wiki(dumpPath,ResumePos,onPage);
+    wikiDumper = new wiki(dumpPath,ResumePos,onPage);
   },1);
 })
 
-
 function onPage(title,pos,text){
   if(!title) {
+    if(wikiDumper) {
+      wikiDumper.resume();
+    }
     return;
   }
   if(filterTitle(skipTitles,title)) {
     //console.log('onPage::filter out title=<',title,'>');
+    if(wikiDumper) {
+      wikiDumper.resume();
+    }
     return;
   }
   console.log('onPage::title=<',title,'>');
-  let d = new SHA3.SHA3Hash(256);
-  d.update(title);
-  let titleSha = d.digest('hex');
   //console.log('onPage::titleSha=<',titleSha,'>');
-  pushPage2DB(titleSha,text);
-  pushTitle2DB(title,titleSha);
-  pushTitle2DB(ResumePosKey,pos);
+  save2Ipfs(title,text,pos);
 }
 
 
 
+function save2Ipfs(cnTitle,cnText,pos) {
+  let bufText = Buffer.from(cnText, 'utf8');
+  ipfs.files.add(bufText,function(err, result) {
+    if (err) {
+      console.log('save2Ipfs::err=<',err,'>');
+      process.exit(0);
+      return;
+    }
+    //console.log('save2Ipfs::result=<',result,'>');
+    let hash = result[0].hash;
+    //console.log('save2Ipfs::hash=<',hash,'>');
+    //console.log('save2Ipfs::cnTitle=<',cnTitle,'>');
+    //console.log('save2Ipfs::pos=<',pos,'>');
+    if(hash) {
+      pushIpfs2DB(hash,cnTitle);
+      pushIpfs2DB(ResumePosKey,pos);
+      if(wikiDumper) {
+        wikiDumper.resume();
+      }
+    }
+  });
+}
 
 
 
