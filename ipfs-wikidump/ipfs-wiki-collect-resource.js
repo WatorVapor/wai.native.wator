24d8cca648550cb2ac7c11a153df1088bd199cee
Wator Vapor <watorvapor@gmail.com>
2018-05-08 21:09:23 +0900

    Update ipfs-wiki-collect-resource.js

--- ipfs-wikidump/ipfs-wiki-collect-resource.js
+++ ipfs-wikidump/ipfs-wiki-collect-resource.js
@@ -13,6 +13,7 @@ stream.on('data', function (data) {
   //console.log(data.key.toString('utf-8'), '=', data.value.toString('utf-8'));
   console.log('data.key=<',data.key.toString('utf-8'),'>')
   console.log('data.value=<',data.value.toString('utf-8'),'>')
+  stream.pause();
   counter++
 });
 stream.on('error', function (err) {
