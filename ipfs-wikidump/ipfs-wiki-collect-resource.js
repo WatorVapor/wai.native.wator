abcb5c9850470d213824447a401e98070716c954
Wator Vapor <watorvapor@gmail.com>
2018-05-08 21:46:55 +0900

    Update ipfs-wiki-collect-resource.js

--- ipfs-wikidump/ipfs-wiki-collect-resource.js
+++ ipfs-wikidump/ipfs-wiki-collect-resource.js
@@ -37,7 +37,7 @@ stream.on('end', function () {
 
 function readIpfsInfo(path) {
   console.log('readIpfsInfo::path=<',path,'>');
-  ipfs.lsReadableStream('/ipfs/' + path, function (err, files) {
+  ipfs.files.get(path, function (err, files) {
     console.log('readIpfsInfo::files=<',files,'>');
   });
 }
