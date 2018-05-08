de0b9a52529142733928973d74c0de598550fd05
Wator Vapor <watorvapor@gmail.com>
2018-05-08 21:07:26 +0900

    Update ipfs-wiki-collect-resource.js

--- ipfs-wikidump/ipfs-wiki-collect-resource.js
+++ ipfs-wikidump/ipfs-wiki-collect-resource.js
@@ -3,3 +3,25 @@ const dbPath = '/watorvapor/wai.storage/dumps.wikimedia.org/output_leveldb/cnwik
 const level = require('level');
 const ipfsAPI = require('ipfs-api');
 const ipfs = ipfsAPI({host: 'localhost', port: '5002', protocol: 'http'});
+
+
+
+let stream = db.createReadStream();
+var counter = 0;
+stream.on('data', function (data) {
+  //console.log(data.key.toString('utf-8'), '=', data.value.toString('utf-8'));
+  console.log('data.key=<',data.key.toString('utf-8'),'>')
+  console.log('data.value=<',data.value.toString('utf-8'),'>')
+  counter++
+});
+stream.on('error', function (err) {
+  console.log('Oh my!', err)
+});
+stream.on('close', function () {
+  console.log('Stream closed')
+});
+stream.on('end', function () {
+  console.log('Stream ended')
+  console.log('counter=<',counter,'>')
+});
+
