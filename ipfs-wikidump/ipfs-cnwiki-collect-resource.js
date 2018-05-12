d64744f2e0416f1d255cf91fbab9f27eee97f2c2
Wator Vapor <watorvapor@gmail.com>
2018-05-12 15:40:27 +0900

    Update ipfs-cnwiki-collect-resource.js

--- ipfs-wikidump/ipfs-cnwiki-collect-resource.js
+++ ipfs-wikidump/ipfs-cnwiki-collect-resource.js
@@ -57,7 +57,7 @@ function startReadClips() {
   }
   stream.on('data', function (data) {
     //console.log(data.key.toString('utf-8'), '=', data.value.toString('utf-8'));
-    //console.log('data.key=<',data.key.toString('utf-8'),'>');
+    console.log('data.key=<',data.key.toString('utf-8'),'>');
     //console.log('data.value=<',data.value.toString('utf-8'),'>');
     stream.pause();
     readIpfsInfo(data.key.toString('utf-8'));
