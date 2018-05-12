f086036fc7e41c58c47ef811e4b0be96a3664c65
Wator Vapor <watorvapor@gmail.com>
2018-05-12 22:49:32 +0900

    Update jawiki.sh

--- ipfs-wikidump/jawiki.sh
+++ ipfs-wikidump/jawiki.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /watorvapor/wai.storage/wai.native.wator/ipfs-wikidump && nodejs ipfs-jawiki.js >> log.ipfs-jawiki.js.log
+cd /watorvapor/wai.storage/wai.native.wator/ipfs-wikidump && nodejs ipfs-jawiki-collect-resource.js >> log.ipfs-jawiki-collect-resource.js.log
