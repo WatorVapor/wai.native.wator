484f0c46ae85da165a5f4e913250c9de93e89aa0
Wator Vapor <watorvapor@gmail.com>
2018-05-21 21:38:01 +0900

    text pump

---
+++ watorword/ipfspump.cpp
@@ -0,0 +1,91 @@
+
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <map>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <vector>
+using namespace std;
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include "textpump.hpp"
+
+TextPump::TextPump() {}
+TextPump::TextPump(const string &dir) : dir_(dir) {}
+TextPump::TextPump(const string &url, const string &tag)
+    : dir_(""), url_(url), tag_(tag) {
+    
+  auto uuid = boost::uuids::random_generator()();
+  ws_ = "/tmp/wai.native/" + boost::lexical_cast<std::string>(uuid);
+  string cmd = "mkdir -p ";
+        cmd += ws_;
+  ::system(cmd.c_str());
+    
+    }
+TextPump::~TextPump() {}
+string TextPump::statistics(void) {
+  std::stringstream ss;
+  ss << "iTototl=<" << iTototl << ">,iProccessed =<" << iProccessed << ">";
+  return ss.str();
+}
+
+#include <boost/property_tree/xml_parser.hpp>
+namespace pt = boost::property_tree;
+
+bool TextPump::fetchMasterTask(pt::ptree &task, string &content) {
+  string taskJSONPath = ws_ + "/task.json";
+  string taskTextPath = ws_ + "/task.text";
+  string wget = "wget -6 --tries=3 --connect-timeout=10 \"";
+  // string wget =  "wget ";
+  wget += url_;
+  wget += "/";
+  wget += tag_;
+  wget += "\" -O ";
+  wget += taskJSONPath;
+  DUMP_VAR(wget);
+  ::system(wget.c_str());
+  try {
+    pt::ptree taskJson;
+    pt::read_json(taskJSONPath, taskJson);
+    task = taskJson;
+    string taskURL;
+    auto taskURLOpt = taskJson.get_optional<string>("url");
+    if (taskURLOpt) {
+      taskURL = taskURLOpt.get();
+      DUMP_VAR(taskURL);
+      string textWget = "wget --tries=3 --connect-timeout=10 \"";
+      textWget += taskURL;
+      textWget += "\" -O ";
+      textWget += taskTextPath;
+      DUMP_VAR(textWget);
+      ::system(textWget.c_str());
+      std::ifstream textStream(taskTextPath);
+      std::string str((std::istreambuf_iterator<char>(textStream)),
+                      std::istreambuf_iterator<char>());
+      textStream.close();
+      content = str;
+      /*
+            pt::ptree textXml;
+            pt::read_xml(taskTextPath,textXml);
+            std::stringstream ss;
+            const int indent = 2;
+            pt::write_xml(ss,textXml,std::locale(),pt::xml_writer_make_settings('
+         ', indent, widen<char>("utf-8")));
+            content = ss.str();
+      */
+    }
+  } catch (const pt::json_parser::json_parser_error &e) {
+    DUMP_VAR(e.what());
+    return false;
+  } catch (const std::exception &ex) {
+    DUMP_VAR(ex.what());
+    return false;
+  }
+  return true;
+}
---
+++ watorword/ipfspump.hpp
@@ -0,0 +1,156 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
+using namespace std;
+#pragma once
+
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+#include <boost/foreach.hpp>
+#include <boost/optional.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+namespace pt = boost::property_tree;
+#include "log.hpp"
+
+
+class TextPump {
+ public:
+  TextPump();
+  TextPump(const string &dir);
+  TextPump(const string &url, const string &tag);
+  ~TextPump();
+  string statistics(void);
+  // loop
+  template <typename T>
+  void eachTextFile(const string &dir, T fn) {
+    const fs::path path(dir);
+    BOOST_FOREACH (const fs::path &p,
+                   std::make_pair(fs::recursive_directory_iterator(path),
+                                  fs::recursive_directory_iterator())) {
+      if (!fs::is_directory(p)) {
+        auto extension = p.extension().string();
+        TRACE_VAR(extension);
+        if (extension == ".txt") {
+          string pathText = p.string();
+          TRACE_VAR(pathText);
+          std::ifstream textStream(pathText);
+          std::string str((std::istreambuf_iterator<char>(textStream)),
+                          std::istreambuf_iterator<char>());
+          fn(pathText, str);
+          textStream.close();
+        }
+      }
+    }
+  }
+  template <typename T>
+  void eachJsonFile(const string &dir, T fn) {
+    const fs::path path(dir);
+    BOOST_FOREACH (const fs::path &p,
+                   std::make_pair(fs::recursive_directory_iterator(path),
+                                  fs::recursive_directory_iterator())) {
+      if (!fs::is_directory(p)) {
+        auto extension = p.extension().string();
+        TRACE_VAR(extension);
+        if (extension == ".json") {
+          string pathText = p.string();
+          fn(pathText);
+        }
+      }
+    }
+  }
+  template <typename T>
+  void eachJsonFile(T fn) {
+    const fs::path path(dir_);
+    BOOST_FOREACH (const fs::path &p,
+                   std::make_pair(fs::recursive_directory_iterator(path),
+                                  fs::recursive_directory_iterator())) {
+      if (!fs::is_directory(p)) {
+        auto extension = p.extension().string();
+        TRACE_VAR(extension);
+        if (extension == ".json") {
+          string pathText = p.string();
+          fn(pathText);
+        }
+      }
+    }
+  }
+  template <typename T>
+  void eachNewText(const string &tag, T fn) {
+    const fs::path path(dir_);
+    BOOST_FOREACH (const fs::path &p,
+                   std::make_pair(fs::recursive_directory_iterator(path),
+                                  fs::recursive_directory_iterator())) {
+      if (!fs::is_directory(p)) {
+        auto extension = p.extension().string();
+        TRACE_VAR(extension);
+        if (extension == ".json") {
+          string pathText = p.string();
+          parseMeta(pathText, tag, fn);
+          iTototl++;
+        }
+      }
+    }
+  }
+  template <typename T>
+  void eachTextFromMaster(T fn) {
+    DUMP_VAR2(url_, tag_);
+    while (true) {
+      pt::ptree task;
+      string content;
+      string ws;
+      if (fetchMasterTask(task, content)) {
+        fn(task, content,ws_);
+      }
+    }
+  }
+
+ private:
+  template <typename T>
+  void parseMeta(const string &pathMeata, const string &tag, T fn) {
+    try {
+      pt::ptree textMeta;
+      pt::read_json(pathMeata, textMeta);
+      string pathText;
+      auto textPathOpt = textMeta.get_optional<string>("path");
+      if (textPathOpt) {
+        pathText = textPathOpt.get();
+      }
+      if (pathText.empty() == false) {
+        auto tagOpt = textMeta.get_optional<bool>(tag);
+        if (tagOpt) {
+          DUMP_VAR3(tagOpt.get(), pathMeata, pathText);
+        } else {
+          TRACE_VAR(pathMeata, pathText);
+          std::ifstream textStream(prefix_ + "/" + pathText);
+          TRACE_VAR(textStream.good());
+          if (textStream.good()) {
+            std::string str((std::istreambuf_iterator<char>(textStream)),
+                            std::istreambuf_iterator<char>());
+            fn(pathText, str);
+            textStream.close();
+            textMeta.put(tag, true);
+            pt::write_json(pathMeata, textMeta);
+            iProccessed++;
+          }
+        }
+      }
+    } catch (const pt::json_parser::json_parser_error &e) {
+      DUMP_VAR(e.what());
+    } catch (const std::exception &ex) {
+      DUMP_VAR(ex.what());
+    }
+  }
+  bool fetchMasterTask(pt::ptree &task, string &content);
+
+ private:
+  const string dir_;
+  const string url_;
+  const string tag_;
+  const string prefix_ = "url_crawl";
+  string ws_;
+  int iTototl = 0;
+  int iProccessed = 0;
+};
