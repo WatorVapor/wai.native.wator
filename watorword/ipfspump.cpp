7cf3fb74e7a7d5f5ed1c3a8c2dfc35c3d1f2d5a1
Wator Vapor <watorvapor@gmail.com>
2018-05-27 09:48:50 +0900

    Update ipfspump.cpp

--- watorword/ipfspump.cpp
+++ watorword/ipfspump.cpp
@@ -189,58 +189,8 @@ bool IpfsTextPump::fetchMasterTask(pt::ptree &task, string &content) {
     return false;
   }
   auto cid = resoureCIDs_.back();
-  auto text = fetchIpfsResource(cid);
+  content = fetchIpfsResource(cid);
   DUMP_VAR(text);
   resoureCIDs_.pop_back();
-  
-#if 0
-  string taskJSONPath = ws_ + "/task.json";
-  string taskTextPath = ws_ + "/task.text";
-  string wget = "wget -6 --tries=3 --connect-timeout=10 \"";
-  // string wget =  "wget ";
-  wget += url_;
-  wget += "\" -O ";
-  wget += taskJSONPath;
-  DUMP_VAR(wget);
-  ::system(wget.c_str());
-  try {
-    pt::ptree taskJson;
-    pt::read_json(taskJSONPath, taskJson);
-    task = taskJson;
-    string taskURL;
-    auto taskURLOpt = taskJson.get_optional<string>("url");
-    if (taskURLOpt) {
-      taskURL = taskURLOpt.get();
-      DUMP_VAR(taskURL);
-      string textWget = "wget --tries=3 --connect-timeout=10 \"";
-      textWget += taskURL;
-      textWget += "\" -O ";
-      textWget += taskTextPath;
-      DUMP_VAR(textWget);
-      ::system(textWget.c_str());
-      std::ifstream textStream(taskTextPath);
-      std::string str((std::istreambuf_iterator<char>(textStream)),
-                      std::istreambuf_iterator<char>());
-      textStream.close();
-      content = str;
-      /*
-            pt::ptree textXml;
-            pt::read_xml(taskTextPath,textXml);
-            std::stringstream ss;
-            const int indent = 2;
-            pt::write_xml(ss,textXml,std::locale(),pt::xml_writer_make_settings('
-         ', indent, widen<char>("utf-8")));
-            content = ss.str();
-      */
-    }
-  } catch (const pt::json_parser::json_parser_error &e) {
-    DUMP_VAR(e.what());
-    return false;
-  } catch (const std::exception &ex) {
-    DUMP_VAR(ex.what());
-    return false;
-  }
-#endif
-  
   return true;
 }
