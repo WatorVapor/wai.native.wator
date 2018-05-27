993d58d30f7a19df422f1064180bc136ffa5ef85
Wator Vapor <watorvapor@gmail.com>
2018-05-27 09:40:48 +0900

    Update ostrichword.db.cpp

--- watorword/ostrichword.db.cpp
+++ watorword/ostrichword.db.cpp
@@ -40,3 +40,31 @@ void OstrichWord::commitArticle(const pt::ptree &task,const string &ws) {
     }
   }
 }
+
+void OstrichWord::commitArticleIpfs(const pt::ptree &task,const string &ws) {
+  auto wordArrays = pickupWordRanking();
+  multiWordOfOneArticle_.clear();
+  for (auto word : wordArrays) {
+    DUMP_VAR(word);
+    pt::ptree upTask = task;
+    upTask.put("word", word);
+    string task_word_upPath = ws + "/task_word_up.json";
+    pt::write_json(task_word_upPath, upTask);
+
+    auto tagOpt = task.get_optional<string>("tag");
+    if (tagOpt) {
+      auto tag = tagOpt.get();
+      string wgetTaskUp("curl -6 -F \"");
+      // string wgetTaskUp("curl -F \"");
+      wgetTaskUp += "file=@";
+      wgetTaskUp += task_word_upPath;
+      wgetTaskUp += "\" ";
+      wgetTaskUp += "\"https://www.wator.xyz/wai/text/train/ostrich/";
+      wgetTaskUp += tag;
+      wgetTaskUp += "\"";
+      DUMP_VAR(wgetTaskUp);
+      ::system(wgetTaskUp.c_str());
+    }
+  }
+}
+
