9e4ca97a6028163fd2b84fccdffd33f948900b5d
Wator Vapor <watorvapor@gmail.com>
2018-05-27 09:29:50 +0900

    Update ipfspump.hpp

--- watorword/ipfspump.hpp
+++ watorword/ipfspump.hpp
@@ -38,7 +38,7 @@ class IpfsTextPump {
  private:
   string ws_;
   const string url_ = "https://ipfs.wator.xyz/ipfs";
-  vector<string> resoureCIDs_;
+  vector<string> resoureCIDs_ = {};
   int iTototl = 0;
   int iProccessed = 0;
 };
