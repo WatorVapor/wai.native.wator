13f5d6abf22c5e3ef01dec743fa276cbad0dcd6a
Wator Vapor <watorvapor@gmail.com>
2018-07-05 22:39:02 +0900

    Update zhwiki.sh

--- ipfs-wikidump/zhwiki.sh
+++ ipfs-wikidump/zhwiki.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /watorvapor/wai.storage/wai.native.wator/ipfs-wikidump && nodejs zhwiki.js >> log.zhwiki.js.log
+cd /watorvapor/wai.storage/wai.native.wator/ipfs-wikidump && nodejs ipfs-cnwiki-collect-resource.js >> ipfs-cnwiki-collect-resource.js.log
