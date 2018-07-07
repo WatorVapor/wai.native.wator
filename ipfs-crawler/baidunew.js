7831c956754be55233869fb21f319caebb9f5d1b
Wator Vapor <watorvapor@gmail.com>
2018-07-07 17:14:35 +0900

    Update baidunew.js

--- ipfs-crawler/baidunew.js
+++ ipfs-crawler/baidunew.js
@@ -1,1 +1,13 @@
-http://news.baidu.com
+const request = require('request');
+const cheerio = require('cheerio');
+
+const requestParam = {
+  url: "http://news.baidu.com"
+};
+request.get(requestParam,onHttpRequest);
+
+onHttpRequest(error, response, body) {
+  console.log('onHttpRequest::error=<',error,'>');
+  console.log('onHttpRequest::response=<',response,'>');
+  console.log('onHttpRequest::body=<',body,'>');
+}
