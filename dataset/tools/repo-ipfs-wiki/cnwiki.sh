67df59267650e7f3c1a495af24d18e9faeaf88fb
Wator Vapor <watorvapor@gmail.com>
2019-03-10 14:57:17 +0000

    local ipfs

--- dataset/tools/repo-ipfs-wiki/cnwiki.sh
+++ dataset/tools/repo-ipfs-wiki/cnwiki.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #cd /watorvapor/wai.storage/wai.native.wator/ipfs-wikidump && nodejs ipfs-cnwiki-collect-resource.js >> log.ipfs-cnwiki-collect-resource.js.log
 
-cd /watorvapor/workspace/node3.wai.native.wator/dataset/tools/ipfs-wikidump && \
+cd /ceph/storage3/ipfs/wai.native.wator/dataset/tools/repo-ipfs-wiki && \
 nodejs ipfs-cnwiki.js >> log.ipfs-cnwiki.js.log
 
