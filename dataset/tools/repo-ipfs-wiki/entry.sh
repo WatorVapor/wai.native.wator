76378a2f2f9f4ec07bf358362419efe779b5c9ae
Wator Vapor <watorvapor@gmail.com>
2019-03-11 22:45:49 +0900

    Update entry.sh

--- dataset/tools/repo-ipfs-wiki/entry.sh
+++ dataset/tools/repo-ipfs-wiki/entry.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/env bash
 npm update --save
-cp -f config-nodejs.js node_modules/ipfs/src/core/runtime/config-nodejs.js
-cp -f init-and-daemon.sh node_modules/ipfs/init-and-daemon.sh
-cd node_modules/ipfs && ./init-and-daemon.sh
+node ipfs-cnwiki.js >> log.ipfs-cnwiki.js.log
