48d687e3916cc86fce5631a31ac61548b1dab670
Wator Vapor <watorvapor@gmail.com>
2019-03-12 13:15:57 +0900

    Update ipfs-cnwiki.js

--- dataset/tools/repo-ipfs-wiki/ipfs-cnwiki.js
+++ dataset/tools/repo-ipfs-wiki/ipfs-cnwiki.js
@@ -1,9 +1,10 @@
 const dumpPath = '/watorvapor/wai.storage/dumps.wikimedia.org/zhwiki/zhwiki-20190201-pages-articles.xml';
-const dbPath = './cnwiki/ipfs';
+const dbPath = '/watorvapor/wai.storage/dumps.wikimedia.org/output_repo/cnwiki/fs';
+const fsPath = '/watorvapor/wai.storage/dumps.wikimedia.org/output_repo/cnwiki/repo';
 const  skipTitles = [
   'Wikipedia:','Help:','Template:','Category:','MediaWiki:','Hex',
   'File:','Portal:','模块:',
 ];
 
 const Main = require('./ipfs-main.js');
-const main = new Main(dumpPath,dbPath,skipTitles);
+const main = new Main(dumpPath,dbPath,fsPath,skipTitles);
