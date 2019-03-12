2162df9040429a8196756d4b07104e74c7047d0a
Wator Vapor <watorvapor@gmail.com>
2019-03-12 13:23:36 +0900

    Update ipfs-levelfs.js

--- dataset/tools/repo-ipfs-wiki/ipfs-levelfs.js
+++ dataset/tools/repo-ipfs-wiki/ipfs-levelfs.js
@@ -6,7 +6,7 @@ module.exports = class IpfsSaveFs {
   constructor(path) {
     this._path = path;
     const result = execSync('mkdir -p ' + this._path);
-    console.log('IpfsSave::constructor result=<',result,'>');
+    console.log('IpfsSaveFs::constructor result=<',result,'>');
     let self = this;
     setTimeout( () => {
       if(typeof self.onReady === 'function') {
@@ -18,8 +18,10 @@ module.exports = class IpfsSaveFs {
     const shasum = crypto.createHash('sha1');
     shasum.update(cnTitle);
     let hash = shasum.digest('hex');
-    let path = this._path + hash.substring(0, 6) + '/';
-    const result =  execSync('mkdir -p ' + hash);
+    console.log('IpfsSaveFs::saveFS hash=<',hash,'>');
+    let path = this._path + '/' + hash.substring(0, 6) + '/';
+    console.log('IpfsSaveFs::saveFS path=<',path,'>');
+    const result =  execSync('mkdir -p ' + path);
     console.log('IpfsSave::save result=<',result,'>');
     fs.writeFileSync(path + 'text',cnText);
     cb(hash,true);
