bdc61443abff48245c9def28aee6f97ef4252cf7
Wator Vapor <watorvapor@gmail.com>
2019-03-13 22:58:32 +0900

    Update init.repos.sh

--- dataset/tools/repo-ipfs-wiki/cnwiki-ipfs/init.repos.sh
+++ dataset/tools/repo-ipfs-wiki/cnwiki-ipfs/init.repos.sh
@@ -4,6 +4,9 @@ wc=/ceph/storage3/ipfs/wai.native.wator/dataset/tools/repo-ipfs-wiki/cnwiki-ipfs
 docker run \
   -v ${wc}/export:/data/export \
   -v ${wc}/storage:/data/ipfs \
-  -e IPFS_PROFILE=server,badgerds \
   -it ipfs/go-ipfs:release \
-   daemon ipfs/go-ipfs init 
+   ipfs init 
+
+
+#  -e IPFS_PROFILE=server,badgerds \
+
