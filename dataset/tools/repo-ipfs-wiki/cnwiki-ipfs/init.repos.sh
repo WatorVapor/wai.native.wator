614ed3f935568548730a1e1fc18d1c54b1a18aa6
Wator Vapor <watorvapor@gmail.com>
2019-03-13 13:25:12 +0000

    local ipfs

--- dataset/tools/repo-ipfs-wiki/cnwiki-ipfs/init.repos.sh
+++ dataset/tools/repo-ipfs-wiki/cnwiki-ipfs/init.repos.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 docker run \
-  -v /opt/dns.wator/service/certbot/etc:/etc/letsencrypt \
-  -v /opt/dns.wator/service/certbot/var:/var/lib/letsencrypt \
-  -v /opt/dns.wator/service/certbot/log:/var/log/letsencrypt \
-  -it certbot/certbot \
-  certonly --manual -d '*.wator.xyz' -d wator.xyz -m 'watorvapor@gmail.com' \
-  --preferred-challenges dns-01 \
-  --server https://acme-v02.api.letsencrypt.org/directory
+  -v ./export:/export \
+  -v ./storage:/data/ipfs \
+  -it ipfs/go-ipfs:release \
+  ipfs init --help
+  
