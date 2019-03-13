fcc13df44aa8d7bb74c2e758080674c13a578b4a
Wator Vapor <watorvapor@gmail.com>
2019-03-13 23:04:40 +0900

    Update add.file.sh

--- dataset/tools/repo-ipfs-wiki/cnwiki-ipfs/add.file.sh
+++ dataset/tools/repo-ipfs-wiki/cnwiki-ipfs/add.file.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-wc=/ceph/storage3/ipfs/wai.native.wator/dataset/tools/repo-ipfs-wiki/cnwiki-ipfs
-
-docker run \
-  -v ${wc}/export:/data/export \
-  -v ${wc}/storage:/data/ipfs \
-  --name ipfs_cnwiki \
-  -it ipfs/go-ipfs:release \
-   daemon --init-profile=server,badgerds
-
-
-#  -e IPFS_PROFILE=server,badgerds \
-
+docker logs -f ipfs_cnwiki
