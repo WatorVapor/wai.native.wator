38077628dfc0a2ce0df92a402900ead9209fdbac
Wator Vapor <watorvapor@gmail.com>
2019-03-14 14:07:26 +0000

    local ipfs

--- dataset/tools/repo-ipfs-wiki/collect-cid-level.js
+++ dataset/tools/repo-ipfs-wiki/collect-cid-level.js
@@ -1,4 +1,6 @@
 const ipfsAPI = require('ipfs-http-client');
+const fs=require("fs");
+
 const strConstAddress = '/ip4/127.0.0.1/tcp/5011';
 
 const node = ipfsAPI(strConstAddress);
@@ -8,4 +10,5 @@ node.refs.local((err,result) => {
     throw err;
   }
   console.log('node.refs.local: result=<',result,'>');
+  fs.writeFileSync('./refs.local.json',JSON.stringify(result,undefined,2));
 })
