f450bc3aa816baa9bdfe3eeb2e1936e9b7237e37
Wator Vapor <watorvapor@gmail.com>
2019-03-14 15:00:19 +0000

    local ipfs

--- dataset/tools/repo-ipfs-wiki/collect-level.js
+++ dataset/tools/repo-ipfs-wiki/collect-level.js
@@ -5,16 +5,14 @@ const Level = require('level');
 const cids = require('./cnwikidump.refs.local.json');
 const dbPath = './cnwiki-cid';
 db = Level(dbPath);
-for(let i = 0;i < cids.length;i++) {
-  let cidObj = cids[i];
-  //console.log('cidObj=<',cidObj,'>');
-  let cid = cidObj.Ref;
-  console.log('cid=<',cid,'>');
-  db.batch()
-    .put(cid, '')
-    .write( (err)=>{
-      if(err) {
-        throw err;
-      }
-    });
+let main = async () => {
+  for(let i = 0;i < cids.length;i++) {
+    let cidObj = cids[i];
+    //console.log('cidObj=<',cidObj,'>');
+    let cid = cidObj.Ref;
+    console.log('cid=<',cid,'>');
+    await db.put(cid, '');
+  }
 }
+
+main();
