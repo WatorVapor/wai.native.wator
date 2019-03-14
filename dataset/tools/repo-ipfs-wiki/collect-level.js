f112810b1046908e628b8c032816e5bc9ec6bb76
Wator Vapor <watorvapor@gmail.com>
2019-03-14 14:48:19 +0000

    local ipfs

---
+++ dataset/tools/repo-ipfs-wiki/collect-level.js
@@ -0,0 +1,11 @@
+const ipfsAPI = require('ipfs-http-client');
+const fs=require("fs");
+const Level = require('level');
+
+const cids = require('./cnwikidump.refs.local.json');
+const dbPath = './cnwiki-cid';
+db = Level(dbPath);
+for(let i = 0;i < cids.length;i++) {
+  let cidObj = cids[i];
+  console.log('cidObj=<',cidObj,'>');
+}
