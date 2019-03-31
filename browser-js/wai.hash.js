f36f0a42fcdcbd0aca1761fde8f351aaecb977d1
Wator Vapor <watovapor@gmail.com>
2019-03-31 11:16:13 +0000

    roulette

---
+++ browser-js/wai.hash.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+  //console.log('onWaiResource CJK_Table=<',CJK_Table,'>');
+  onWaiInit();
+});
+
+onWaiInit = () => {
+  let parrot = new WaiParrot();
+  console.log('onWaiInit parrot=<',parrot,'>');
+  let crystal = new WaiCrystal(parrot);
+  console.log('onWaiInit crystal=<',crystal,'>');
+  crystal.entryBlock = () => {
+    parrot.entryBlock();
+  }
+  crystal.onArticle = (doc) => {
+    parrot.article(doc);
+  }
+  crystal.leaveBlock = () => {
+    parrot.leaveBlock();
+  }
+  let name = shake256('Message to hash',128);
+  console.log('onWaiInit name=<',name,'>');
+}
